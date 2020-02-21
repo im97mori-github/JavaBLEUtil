0ed33490234992a18c0771dabd4b41e71d4f3fb1
im97mori <github@im97mori.org>
2020-02-21 17:32:55 +0900

    1st commit

---
+++ core/src/main/java/org/im97mori/ble/ByteArrayInterface.java
@@ -0,0 +1,15 @@
+package org.im97mori.ble;
+
+import androidx.annotation.NonNull;
+
+public interface ByteArrayInterface {
+
+	/**
+	 * Create byte array for {@link android.bluetooth.BluetoothGattCharacteristic#setValue(byte[])} or {@link android.bluetooth.BluetoothGattDescriptor#setValue(byte[])}
+	 *
+	 * @return byte array
+	 */
+	@NonNull
+	byte[] getBytes();
+
+}

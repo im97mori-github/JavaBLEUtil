7fdce3420334ae5d9b7d70171d7890a4a69b3a2e
im97mori <github@im97mori.org>
2020-02-25 14:05:29 +0900

    add cscs characteristic

---
+++ characteristic/u2a5b/src/main/java/org/im97mori/ble/characteristic/u2a5b/CSCMeasurement.java
@@ -0,0 +1,225 @@
+package org.im97mori.ble.characteristic.u2a5b;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.ByteArrayInterface;
+
+import androidx.annotation.NonNull;
+
+/**
+ * CSC Measurement (Characteristics UUID: 0x2A5B)
+ */
+public class CSCMeasurement implements ByteArrayInterface {
+
+    /**
+     * @see #FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_FALSE
+     * @see #FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_TRUE
+     */
+    public static final int FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_MASK = 0b00000001;
+
+    /**
+     * 0: Wheel Revolution Data Present False
+     */
+    public static final int FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_FALSE = 0b00000000;
+
+    /**
+     * 1: Wheel Revolution Data Present True
+     */
+    public static final int FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_TRUE = 0b00000001;
+
+    /**
+     * @see #FLAGS_CRANK_REVOLUTION_DATA_PRESENT_FALSE
+     * @see #FLAGS_CRANK_REVOLUTION_DATA_PRESENT_TRUE
+     */
+    public static final int FLAGS_CRANK_REVOLUTION_DATA_PRESENT_MASK = 0b00000010;
+
+    /**
+     * 0: Crank Revolution Data Present False
+     */
+    public static final int FLAGS_CRANK_REVOLUTION_DATA_PRESENT_FALSE = 0b00000000;
+
+    /**
+     * 1: Crank Revolution Data Present True
+     */
+    public static final int FLAGS_CRANK_REVOLUTION_DATA_PRESENT_TRUE = 0b00000010;
+
+    /**
+     * Last Wheel Event Time Unit 1/1024s
+     */
+    public static final double LAST_WHEEL_EVENT_TIME_RESOLUTION = 1 / 1024d;
+
+    /**
+     * Last Crank Event Time Unit 1/1024s
+     */
+    public static final double LAST_CRANK_EVENT_TIME_RESOLUTION = 1 / 1024d;
+
+    /**
+     * Flags
+     */
+    private final int mFlags;
+
+    /**
+     * Cumulative Wheel Revolutions
+     */
+    private final long mCumulativeWheelRevolutions;
+
+    /**
+     * Last Wheel Event Time
+     */
+    private final int mLastWheelEventTime;
+
+    /**
+     * Cumulative Crank Revolutions
+     */
+    private final int mCumulativeCrankRevolutions;
+
+    /**
+     * Last Crank Event Time
+     */
+    private final int mLastCrankEventTime;
+
+    /**
+     * Constructor from {@link BluetoothGattCharacteristic}
+     *
+     * @param bluetoothGattCharacteristic Characteristics UUID: 0x2A5B
+     */
+    public CSCMeasurement(@NonNull byte[] values) {
+        int index = 0;
+        mFlags = values[index++];
+        if (isFlagsWheelRevolutionDataPresent()) {
+            mCumulativeWheelRevolutions = BLEUtils.createUInt32(values, index);
+            index += 4;
+            mLastWheelEventTime = BLEUtils.createUInt16(values, index);
+            index += 2;
+        } else {
+            mCumulativeWheelRevolutions = 0;
+            mLastWheelEventTime = 0;
+        }
+        if (isFlagsCrankRevolutionDataPresent()) {
+            mCumulativeCrankRevolutions = BLEUtils.createUInt16(values, index);
+            index += 2;
+            mLastCrankEventTime = BLEUtils.createUInt16(values, index);
+        } else {
+            mCumulativeCrankRevolutions = 0;
+            mLastCrankEventTime = 0;
+        }
+    }
+
+    /**
+     * @return Flags
+     */
+    public int getFlags() {
+        return mFlags;
+    }
+
+    /**
+     * @return {@code true}:Wheel Revolution Data not Present, {@code false}:Wheel Revolution Data Present
+     */
+    public boolean isFlagsWheelRevolutionDataNotPresent() {
+        return isFlagsMatched(FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_MASK, FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_FALSE);
+    }
+
+    /**
+     * @return {@code true}:Wheel Revolution Data Present, {@code false}:Wheel Revolution Data not Present
+     */
+    public boolean isFlagsWheelRevolutionDataPresent() {
+        return isFlagsMatched(FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_MASK, FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_TRUE);
+    }
+
+    /**
+     * @return {@code true}:Crank Revolution Data not Present, {@code false}:Crank Revolution Data Present
+     */
+    public boolean isFlagsCrankRevolutionDataNotPresent() {
+        return isFlagsMatched(FLAGS_CRANK_REVOLUTION_DATA_PRESENT_MASK, FLAGS_CRANK_REVOLUTION_DATA_PRESENT_FALSE);
+    }
+
+    /**
+     * @return {@code true}:Crank Revolution Data Present, {@code false}:Crank Revolution Data not Present
+     */
+    public boolean isFlagsCrankRevolutionDataPresent() {
+        return isFlagsMatched(FLAGS_CRANK_REVOLUTION_DATA_PRESENT_MASK, FLAGS_CRANK_REVOLUTION_DATA_PRESENT_TRUE);
+    }
+
+    /**
+     * @return Cumulative Wheel Revolutions
+     */
+    public long getCumulativeWheelRevolutions() {
+        return mCumulativeWheelRevolutions;
+    }
+
+    /**
+     * @return Last Wheel Event Time
+     */
+    public int getLastWheelEventTime() {
+        return mLastWheelEventTime;
+    }
+
+    /**
+     * @return Last Wheel Event Time(second)
+     */
+    public double getLastWheelEventTimeSecond() {
+        return LAST_WHEEL_EVENT_TIME_RESOLUTION * mLastWheelEventTime;
+    }
+
+    /**
+     * @return Cumulative Crank Revolutions
+     */
+    public int getCumulativeCrankRevolutions() {
+        return mCumulativeCrankRevolutions;
+    }
+
+    /**
+     * @return Last Crank Event Time
+     */
+    public int getLastCrankEventTime() {
+        return mLastCrankEventTime;
+    }
+
+    /**
+     * @return Last Crank Event Time(second)
+     */
+    public double getLastCrankEventTimeSecond() {
+        return LAST_CRANK_EVENT_TIME_RESOLUTION * mLastCrankEventTime;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @NonNull
+    public byte[] getBytes() {
+        int length = 1;
+        byte[] data = new byte[11];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mFlags);
+        if (isFlagsWheelRevolutionDataPresent()) {
+            byteBuffer.putInt((int) mCumulativeWheelRevolutions);
+            byteBuffer.putShort((short) mLastWheelEventTime);
+            length += 6;
+        }
+        if (isFlagsCrankRevolutionDataPresent()) {
+            byteBuffer.putShort((short) mCumulativeCrankRevolutions);
+            byteBuffer.putShort((short) mLastCrankEventTime);
+            length += 4;
+        }
+        return Arrays.copyOfRange(data, 0, length);
+    }
+
+    /**
+     * check Flags
+     *
+     * @param mask   bitmask for expect
+     * @param expect one of {@link #FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_FALSE}
+     *               , {@link #FLAGS_WHEEL_REVOLUTION_DATA_PRESENT_TRUE}
+     *               , {@link #FLAGS_CRANK_REVOLUTION_DATA_PRESENT_FALSE}
+     *               , {@link #FLAGS_CRANK_REVOLUTION_DATA_PRESENT_TRUE}
+     * @return {@code true}:same as expect, {@code false}:not match
+     */
+    private boolean isFlagsMatched(int mask, int expect) {
+        return (mask & mFlags) == expect;
+    }
+
+}
---
+++ characteristic/u2a5c/src/main/java/org/im97mori/ble/characteristic/u2a5c/CSCFeature.java
@@ -0,0 +1,156 @@
+package org.im97mori.ble.characteristic.u2a5c;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.ByteArrayInterface;
+
+import androidx.annotation.NonNull;
+
+/**
+ * CSC Feature (Characteristics UUID: 0x2A5C)
+ */
+public class CSCFeature implements ByteArrayInterface {
+
+    /**
+     * @see #CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_FALSE
+     * @see #CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_TRUE
+     */
+    public static final int CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_MASK = 0b00000000_00000001;
+
+    /**
+     * 0: Wheel Revolution Data Supported False
+     */
+    public static final int CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_FALSE = 0b00000000_00000000;
+
+    /**
+     * 1: Wheel Revolution Data Supported True
+     */
+    public static final int CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_TRUE = 0b00000000_00000001;
+
+    /**
+     * @see #CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_FALSE
+     * @see #CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_TRUE
+     */
+    public static final int CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_MASK = 0b00000000_00000010;
+
+    /**
+     * 0: Crank Revolution Data Supported False
+     */
+    public static final int CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_FALSE = 0b00000000_00000000;
+
+    /**
+     * 1: Crank Revolution Data Supported True
+     */
+    public static final int CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_TRUE = 0b00000000_00000010;
+
+    /**
+     * @see #CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_FALSE
+     * @see #CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_TRUE
+     */
+    public static final int CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_MASK = 0b00000000_00000100;
+
+    /**
+     * 0: Multiple Sensor Locations Supported False
+     */
+    public static final int CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_FALSE = 0b00000000_00000000;
+
+    /**
+     * 1: Multiple Sensor Locations Supported True
+     */
+    public static final int CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_TRUE = 0b00000000_00000100;
+
+    /**
+     * CSC Feature
+     */
+    private final byte[] mCscFeature;
+
+    /**
+     * Constructor from {@link BluetoothGattCharacteristic}
+     *
+     * @param bluetoothGattCharacteristic Characteristics UUID: 0x2A5C
+     */
+    public CSCFeature(@NonNull byte[] values) {
+        mCscFeature = Arrays.copyOfRange(values, 0, 2);
+    }
+
+    /**
+     * @return CSC Feature
+     */
+    public byte[] getCscFeature() {
+        return mCscFeature;
+    }
+
+    /**
+     * @return {@code true}:Wheel Revolution Data not Supported, {@code false}:Wheel Revolution Data Supported
+     */
+    public boolean isCscFeatureWheelRevolutionDataNotSupported() {
+        return isFeatureMatched(CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_MASK, CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_FALSE);
+    }
+
+    /**
+     * @return {@code true}:Wheel Revolution Data Supported, {@code false}:Wheel Revolution Data not Supported
+     */
+    public boolean isCscFeatureWheelRevolutionDataSupported() {
+        return isFeatureMatched(CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_MASK, CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_TRUE);
+    }
+
+    /**
+     * @return {@code true}:Crank Revolution Data not Supported, {@code false}:Crank Revolution Data Supported
+     */
+    public boolean isCscFeatureCrankRevolutionDataSupportedNotSupported() {
+        return isFeatureMatched(CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_MASK, CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_FALSE);
+    }
+
+    /**
+     * @return {@code true}:Crank Revolution Data Supported, {@code false}:Crank Revolution Data not Supported
+     */
+    public boolean isCscFeatureCrankRevolutionDataSupportedSupported() {
+        return isFeatureMatched(CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_MASK, CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_TRUE);
+    }
+
+    /**
+     * @return {@code true}:Multiple Sensor Locations not Supported, {@code false}:Multiple Sensor Locations Supported
+     */
+    public boolean isCscFeatureMultipleSensorLocationsSupportedNotSupported() {
+        return isFeatureMatched(CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_MASK, CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_FALSE);
+    }
+
+    /**
+     * @return {@code true}:Multiple Sensor Locations Supported, {@code false}:Multiple Sensor Locations not Supported
+     */
+    public boolean isCscFeatureMultipleSensorLocationsSupportedSupported() {
+        return isFeatureMatched(CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_MASK, CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_TRUE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @NonNull
+    public byte[] getBytes() {
+        byte[] data = new byte[2];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put(mCscFeature);
+        return data;
+    }
+
+    /**
+     * check CSC Feature
+     *
+     * @param mask   bitmask for expect
+     * @param expect one of {@link #CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_FALSE}
+     *               , {@link #CSC_FEATURE_WHEEL_REVOLUTION_DATA_SUPPORTED_TRUE}
+     *               , {@link #CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_FALSE}
+     *               , {@link #CSC_FEATURE_CRANK_REVOLUTION_DATA_SUPPORTED_TRUE}
+     *               , {@link #CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_FALSE}
+     *               , {@link #CSC_FEATURE_MULTIPLE_SENSOR_LOCATIONS_SUPPORTED_TRUE}
+     * @return {@code true}:same as expect, {@code false}:not match
+     */
+    private boolean isFeatureMatched(int mask, int expect) {
+        return (mask & BLEUtils.createSInt16(mCscFeature, 0)) == expect;
+    }
+
+}

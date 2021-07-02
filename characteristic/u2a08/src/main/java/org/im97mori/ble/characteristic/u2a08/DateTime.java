1ad2b9d7d3fbef1e8394c75e494e8c5cc998dd01
im97mori <github@im97mori.org>
2021-07-03 07:04:17 +0900

    update Date Time characteristic

--- characteristic/u2a08/src/main/java/org/im97mori/ble/characteristic/u2a08/DateTime.java
+++ characteristic/u2a08/src/main/java/org/im97mori/ble/characteristic/u2a08/DateTime.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2a08;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,37 @@ import androidx.annotation.NonNull;
 /**
  * Date Time (Characteristics UUID: 0x2A08)
  */
-// TODO
 public class DateTime implements ByteArrayInterface {
+
+    /**
+     * Year
+     */
+    private final int mYear;
+
+    /**
+     * Month
+     */
+    private final int mMonth;
+
+    /**
+     * Day
+     */
+    private final int mDay;
+
+    /**
+     * Hours
+     */
+    private final int mHours;
+
+    /**
+     * Minutes
+     */
+    private final int mMinutes;
+
+    /**
+     * Seconds
+     */
+    private final int mSeconds;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +49,73 @@ public class DateTime implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2A08
      */
     public DateTime(@NonNull byte[] values) {
+        mYear = BLEUtils.createUInt16(values, 0);
+        mMonth = (values[2] & 0xff);
+        mDay = (values[3] & 0xff);
+        mHours = (values[4] & 0xff);
+        mMinutes = (values[5] & 0xff);
+        mSeconds = (values[6] & 0xff);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param year      Year
+     * @param month     Month
+     * @param day       Day
+     * @param hours     Hours
+     * @param minutes   Minutes
+     * @param seconds   Seconds
+     */
+    public DateTime(int year, int month, int day, int hours, int minutes, int seconds) {
+        mYear = year;
+        mMonth = month;
+        mDay = day;
+        mHours = hours;
+        mMinutes = minutes;
+        mSeconds = seconds;
+    }
+
+    /**
+     * @return Year
+     */
+    public int getYear() {
+        return mYear;
+    }
+
+    /**
+     * @return Month
+     */
+    public int getMonth() {
+        return mMonth;
+    }
+
+    /**
+     * @return Day
+     */
+    public int getDay() {
+        return mDay;
+    }
+
+    /**
+     * @return Hours
+     */
+    public int getHours() {
+        return mHours;
+    }
+
+    /**
+     * @return Minutes
+     */
+    public int getMinutes() {
+        return mMinutes;
+    }
+
+    /**
+     * @return Seconds
+     */
+    public int getSeconds() {
+        return mSeconds;
     }
 
     /**
@@ -24,7 +124,14 @@ public class DateTime implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[7];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.putShort((short) mYear);
+        byteBuffer.put((byte) mMonth);
+        byteBuffer.put((byte) mDay);
+        byteBuffer.put((byte) mHours);
+        byteBuffer.put((byte) mMinutes);
+        byteBuffer.put((byte) mSeconds);
         return data;
     }
 

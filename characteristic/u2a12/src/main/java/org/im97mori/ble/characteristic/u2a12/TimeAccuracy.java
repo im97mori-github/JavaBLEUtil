e049624c3751403493166339cfba045752d91382
im97mori <github@im97mori.org>
2021-07-04 15:41:20 +0900

    update Time Accuracy characteristic

--- characteristic/u2a12/src/main/java/org/im97mori/ble/characteristic/u2a12/TimeAccuracy.java
+++ characteristic/u2a12/src/main/java/org/im97mori/ble/characteristic/u2a12/TimeAccuracy.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2a12;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,27 @@ import androidx.annotation.NonNull;
 /**
  * Time Accuracy (Characteristics UUID: 0x2A12)
  */
-// TODO
 public class TimeAccuracy implements ByteArrayInterface {
+
+    /**
+     * 254(0xfe): Accuracy out of range
+     */
+    public static final int ACCURACY_OUT_OF_RANGE = 254;
+
+    /**
+     * 255(0xff): Accuracy Unknown
+     */
+    public static final int ACCURACY_UNKNOWN = 255;
+
+    /**
+     * Accuracy unit(millis)
+     */
+    public static final int ACCURACY_UNIT = 125;
+
+    /**
+     * Accuracy
+     */
+    private final int mAccuracy;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +39,46 @@ public class TimeAccuracy implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2A12
      */
     public TimeAccuracy(@NonNull byte[] values) {
+        mAccuracy = BLEUtils.createUInt8(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param accuracy Accuracy
+     */
+    public TimeAccuracy(int accuracy) {
+        mAccuracy = accuracy;
+    }
+
+    /**
+     * @return Accuracy
+     */
+    public int getAccuracy() {
+        return mAccuracy;
+    }
+
+    /**
+     * @return {@code true}:Accuracy is out of range, {@code false}:Accuracy is not out of range
+     * @see #ACCURACY_OUT_OF_RANGE
+     */
+    public boolean isAccuracyOutOfRange() {
+        return ACCURACY_OUT_OF_RANGE == mAccuracy;
+    }
+
+    /**
+     * @return {@code true}:Accuracy is unknown, {@code false}:Accuracy is not unknown
+     * @see #ACCURACY_UNKNOWN
+     */
+    public boolean isAccuracyUnknown() {
+        return ACCURACY_UNKNOWN == mAccuracy;
+    }
+
+    /**
+     * @returnAccuracy(millis)
+     */
+    public int getAccuracyMillis() {
+        return mAccuracy * ACCURACY_UNIT;
     }
 
     /**
@@ -24,7 +87,9 @@ public class TimeAccuracy implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[1];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mAccuracy);
         return data;
     }
 
---
+++ characteristic/u2a12/src/test/java/org/im97mori/ble/characteristic/u2a12/TimeAccuracyTest.java
@@ -0,0 +1,85 @@
+package org.im97mori.ble.characteristic.u2a12;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TimeAccuracyTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeAccuracy.ACCURACY_OUT_OF_RANGE;
+        //@formatter:on
+
+        TimeAccuracy result1 = new TimeAccuracy(data);
+        assertEquals(TimeAccuracy.ACCURACY_OUT_OF_RANGE, result1.getAccuracy());
+        assertTrue(result1.isAccuracyOutOfRange());
+        assertFalse(result1.isAccuracyUnknown());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeAccuracy.ACCURACY_UNKNOWN;
+        //@formatter:on
+
+        TimeAccuracy result1 = new TimeAccuracy(data);
+        assertEquals(TimeAccuracy.ACCURACY_UNKNOWN, result1.getAccuracy());
+        assertFalse(result1.isAccuracyOutOfRange());
+        assertTrue(result1.isAccuracyUnknown());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = 0;
+        //@formatter:on
+
+        TimeAccuracy result1 = new TimeAccuracy(data);
+        assertEquals(0, result1.getAccuracy());
+        assertFalse(result1.isAccuracyOutOfRange());
+        assertFalse(result1.isAccuracyUnknown());
+        assertEquals(0 * TimeAccuracy.ACCURACY_UNIT, result1.getAccuracyMillis());
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) 253;
+        //@formatter:on
+
+        TimeAccuracy result1 = new TimeAccuracy(data);
+        assertEquals(253, result1.getAccuracy());
+        assertFalse(result1.isAccuracyOutOfRange());
+        assertFalse(result1.isAccuracyUnknown());
+        assertEquals(253 * TimeAccuracy.ACCURACY_UNIT, result1.getAccuracyMillis());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int accuracy = 1;
+
+        TimeAccuracy result1 = new TimeAccuracy(accuracy);
+        assertEquals(accuracy, result1.getAccuracy());
+    }
+
+    @Test
+    public void test_parcelable_00100() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = 0x01;
+        //@formatter:on
+
+        TimeAccuracy result1 = new TimeAccuracy(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

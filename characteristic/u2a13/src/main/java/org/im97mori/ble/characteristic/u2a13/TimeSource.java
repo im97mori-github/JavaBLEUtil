95525c7bf7503d4e0ca8ce97d19499e01261a38c
im97mori <github@im97mori.org>
2021-07-04 17:15:40 +0900

    update Time Source characteristic

--- characteristic/u2a13/src/main/java/org/im97mori/ble/characteristic/u2a13/TimeSource.java
+++ characteristic/u2a13/src/main/java/org/im97mori/ble/characteristic/u2a13/TimeSource.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2a13;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,47 @@ import androidx.annotation.NonNull;
 /**
  * Time Source (Characteristics UUID: 0x2A13)
  */
-// TODO
 public class TimeSource implements ByteArrayInterface {
+
+    /**
+     * 0: Time Source Unknown
+     */
+    public static final int TIME_SOURCE_UNKNOWN = 0;
+
+    /**
+     * 1: Time Source Network Time Protocol
+     */
+    public static final int TIME_SOURCE_NETWORK_TIME_PROTOCOL = 1;
+
+    /**
+     * 2: Time Source GPS
+     */
+    public static final int TIME_SOURCE_GPS = 2;
+
+    /**
+     * 3: Time Source Radio Time Signal
+     */
+    public static final int TIME_SOURCE_RADIO_TIME_SIGNAL = 3;
+
+    /**
+     * 4: Time Source Manual
+     */
+    public static final int TIME_SOURCE_MANUAL = 4;
+
+    /**
+     * 5: Time Source Atomic Clock
+     */
+    public static final int TIME_SOURCE_ATOMIC_CLOCK = 5;
+
+    /**
+     * 6: Time Source Cellular Network
+     */
+    public static final int TIME_SOURCE_CELLULAR_NETWORK = 6;
+
+    /**
+     * Time Source
+     */
+    private final int mTimeSource;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +59,79 @@ public class TimeSource implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2A13
      */
     public TimeSource(@NonNull byte[] values) {
+        mTimeSource = BLEUtils.createUInt8(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param timeSource Time Source
+     */
+    public TimeSource(int timeSource) {
+        mTimeSource = timeSource;
+    }
+
+    /**
+     * @return Time Source
+     */
+    public int getTimeSource() {
+        return mTimeSource;
+    }
+
+    /**
+     * @return {@code true}:Time Source is Unknown, {@code false}:Time Source is not Unknown
+     * @see #TIME_SOURCE_UNKNOWN
+     */
+    public boolean isTimeSourceUnknown() {
+        return TIME_SOURCE_UNKNOWN == mTimeSource;
+    }
+
+    /**
+     * @return {@code true}:Time Source is Network Time Protocol, {@code false}:Time Source is not Network Time Protocol
+     * @see #TIME_SOURCE_NETWORK_TIME_PROTOCOL
+     */
+    public boolean isTimeSourceNetworkTimeProtocol() {
+        return TIME_SOURCE_NETWORK_TIME_PROTOCOL == mTimeSource;
+    }
+
+    /**
+     * @return {@code true}:Time Source is GPS, {@code false}:Time Source is not GPS
+     * @see #TIME_SOURCE_GPS
+     */
+    public boolean isTimeSourceNetworkGps() {
+        return TIME_SOURCE_GPS == mTimeSource;
+    }
+
+    /**
+     * @return {@code true}:Time Source is Radio Time Signal, {@code false}:Time Source is not Radio Time Signal
+     * @see #TIME_SOURCE_RADIO_TIME_SIGNAL
+     */
+    public boolean isTimeSourceRadioTimeSignal() {
+        return TIME_SOURCE_RADIO_TIME_SIGNAL == mTimeSource;
+    }
+
+    /**
+     * @return {@code true}:Time Source is Manual, {@code false}:Time Source is not Manual
+     * @see #TIME_SOURCE_MANUAL
+     */
+    public boolean isTimeSourceManual() {
+        return TIME_SOURCE_MANUAL == mTimeSource;
+    }
+
+    /**
+     * @return {@code true}:Time Source is Atomic Clock, {@code false}:Time Source is not Atomic Clock
+     * @see #TIME_SOURCE_ATOMIC_CLOCK
+     */
+    public boolean isTimeSourceAtomicClock() {
+        return TIME_SOURCE_ATOMIC_CLOCK == mTimeSource;
+    }
+
+    /**
+     * @return {@code true}:Time Source is Cellular Network, {@code false}:Time Source is not Cellular Network
+     * @see #TIME_SOURCE_CELLULAR_NETWORK
+     */
+    public boolean isTimeSourceCellularNetwork() {
+        return TIME_SOURCE_CELLULAR_NETWORK == mTimeSource;
     }
 
     /**
@@ -24,7 +140,9 @@ public class TimeSource implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[1];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mTimeSource);
         return data;
     }
 
---
+++ characteristic/u2a13/src/test/java/org/im97mori/ble/characteristic/u2a13/TimeSourceTest.java
@@ -0,0 +1,157 @@
+package org.im97mori.ble.characteristic.u2a13;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TimeSourceTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSource.TIME_SOURCE_UNKNOWN;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertEquals(TimeSource.TIME_SOURCE_UNKNOWN, result1.getTimeSource());
+        assertTrue(result1.isTimeSourceUnknown());
+        assertFalse(result1.isTimeSourceNetworkTimeProtocol());
+        assertFalse(result1.isTimeSourceNetworkGps());
+        assertFalse(result1.isTimeSourceRadioTimeSignal());
+        assertFalse(result1.isTimeSourceManual());
+        assertFalse(result1.isTimeSourceAtomicClock());
+        assertFalse(result1.isTimeSourceCellularNetwork());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSource.TIME_SOURCE_NETWORK_TIME_PROTOCOL;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertEquals(TimeSource.TIME_SOURCE_NETWORK_TIME_PROTOCOL, result1.getTimeSource());
+        assertFalse(result1.isTimeSourceUnknown());
+        assertTrue(result1.isTimeSourceNetworkTimeProtocol());
+        assertFalse(result1.isTimeSourceNetworkGps());
+        assertFalse(result1.isTimeSourceRadioTimeSignal());
+        assertFalse(result1.isTimeSourceManual());
+        assertFalse(result1.isTimeSourceAtomicClock());
+        assertFalse(result1.isTimeSourceCellularNetwork());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSource.TIME_SOURCE_GPS;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertEquals(TimeSource.TIME_SOURCE_GPS, result1.getTimeSource());
+        assertFalse(result1.isTimeSourceUnknown());
+        assertFalse(result1.isTimeSourceNetworkTimeProtocol());
+        assertTrue(result1.isTimeSourceNetworkGps());
+        assertFalse(result1.isTimeSourceRadioTimeSignal());
+        assertFalse(result1.isTimeSourceManual());
+        assertFalse(result1.isTimeSourceAtomicClock());
+        assertFalse(result1.isTimeSourceCellularNetwork());
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSource.TIME_SOURCE_RADIO_TIME_SIGNAL;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertEquals(TimeSource.TIME_SOURCE_RADIO_TIME_SIGNAL, result1.getTimeSource());
+        assertFalse(result1.isTimeSourceUnknown());
+        assertFalse(result1.isTimeSourceNetworkTimeProtocol());
+        assertFalse(result1.isTimeSourceNetworkGps());
+        assertTrue(result1.isTimeSourceRadioTimeSignal());
+        assertFalse(result1.isTimeSourceManual());
+        assertFalse(result1.isTimeSourceAtomicClock());
+        assertFalse(result1.isTimeSourceCellularNetwork());
+    }
+
+    @Test
+    public void test_constructor_00005() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSource.TIME_SOURCE_MANUAL;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertEquals(TimeSource.TIME_SOURCE_MANUAL, result1.getTimeSource());
+        assertFalse(result1.isTimeSourceUnknown());
+        assertFalse(result1.isTimeSourceNetworkTimeProtocol());
+        assertFalse(result1.isTimeSourceNetworkGps());
+        assertFalse(result1.isTimeSourceRadioTimeSignal());
+        assertTrue(result1.isTimeSourceManual());
+        assertFalse(result1.isTimeSourceAtomicClock());
+        assertFalse(result1.isTimeSourceCellularNetwork());
+    }
+
+    @Test
+    public void test_constructor_00006() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSource.TIME_SOURCE_ATOMIC_CLOCK;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertEquals(TimeSource.TIME_SOURCE_ATOMIC_CLOCK, result1.getTimeSource());
+        assertFalse(result1.isTimeSourceUnknown());
+        assertFalse(result1.isTimeSourceNetworkTimeProtocol());
+        assertFalse(result1.isTimeSourceNetworkGps());
+        assertFalse(result1.isTimeSourceRadioTimeSignal());
+        assertFalse(result1.isTimeSourceManual());
+        assertTrue(result1.isTimeSourceAtomicClock());
+        assertFalse(result1.isTimeSourceCellularNetwork());
+    }
+
+    @Test
+    public void test_constructor_00007() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSource.TIME_SOURCE_CELLULAR_NETWORK;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertEquals(TimeSource.TIME_SOURCE_CELLULAR_NETWORK, result1.getTimeSource());
+        assertFalse(result1.isTimeSourceUnknown());
+        assertFalse(result1.isTimeSourceNetworkTimeProtocol());
+        assertFalse(result1.isTimeSourceNetworkGps());
+        assertFalse(result1.isTimeSourceRadioTimeSignal());
+        assertFalse(result1.isTimeSourceManual());
+        assertFalse(result1.isTimeSourceAtomicClock());
+        assertTrue(result1.isTimeSourceCellularNetwork());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int timeZone = 1;
+
+        TimeSource result1 = new TimeSource(timeZone);
+        assertEquals(timeZone, result1.getTimeSource());
+    }
+
+    @Test
+    public void test_parcelable_00100() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = 0x01;
+        //@formatter:on
+
+        TimeSource result1 = new TimeSource(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

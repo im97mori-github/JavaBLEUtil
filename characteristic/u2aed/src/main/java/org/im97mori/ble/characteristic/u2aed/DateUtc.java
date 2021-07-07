7f9a07ea594a322e4415b4e1fbc6a892509486a9
im97mori <github@im97mori.org>
2021-07-07 13:28:43 +0900

    update Date UTC characteristic

--- characteristic/u2aed/src/main/java/org/im97mori/ble/characteristic/u2aed/DateUtc.java
+++ characteristic/u2aed/src/main/java/org/im97mori/ble/characteristic/u2aed/DateUtc.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2aed;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,17 @@ import androidx.annotation.NonNull;
 /**
  * Date UTC (Characteristics UUID: 0x2AED)
  */
-// TODO
 public class DateUtc implements ByteArrayInterface {
+
+    /**
+     * 0x000000 : Date UTC value is not known
+     */
+    public static final int DATE_UTC_IS_NOT_KNOWN = 0x000000;
+
+    /**
+     * Date
+     */
+    private final int mDate;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +29,31 @@ public class DateUtc implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AED
      */
     public DateUtc(@NonNull byte[] values) {
+        mDate = BLEUtils.createUInt24(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param date Date
+     */
+    public DateUtc(int date) {
+        mDate = date;
+    }
+
+    /**
+     * @return Date
+     */
+    public int getDate() {
+        return mDate;
+    }
+
+    /**
+     * @return {@code true}:Date UTC is not known, {@code false}:has Date UTC information
+     * @see #DATE_UTC_IS_NOT_KNOWN
+     */
+    public boolean isDateUtcNotKnown() {
+        return DATE_UTC_IS_NOT_KNOWN == mDate;
     }
 
     /**
@@ -24,7 +62,11 @@ public class DateUtc implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[3];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mDate);
+        byteBuffer.put((byte) (mDate >> 8));
+        byteBuffer.put((byte) (mDate >> 16));
         return data;
     }
 
---
+++ characteristic/u2aed/src/test/java/org/im97mori/ble/characteristic/u2aed/DateUtcTest.java
@@ -0,0 +1,78 @@
+package org.im97mori.ble.characteristic.u2aed;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class DateUtcTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[3];
+        data[ 0] = (byte) DateUtc.DATE_UTC_IS_NOT_KNOWN;
+        data[ 1] = (byte) (DateUtc.DATE_UTC_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) (DateUtc.DATE_UTC_IS_NOT_KNOWN >> 16);
+        //@formatter:on
+
+        DateUtc result1 = new DateUtc(data);
+        assertEquals(DateUtc.DATE_UTC_IS_NOT_KNOWN, result1.getDate());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[3];
+        data[ 0] = (byte) 16777214;
+        data[ 1] = (byte) (16777214 >> 8);
+        data[ 2] = (byte) (16777214 >> 16);
+        //@formatter:on
+
+        DateUtc result1 = new DateUtc(data);
+        assertEquals(16777214, result1.getDate());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int dateUtc = DateUtc.DATE_UTC_IS_NOT_KNOWN;
+
+        DateUtc result1 = new DateUtc(dateUtc);
+        assertEquals(dateUtc, result1.getDate());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int dateUtc = 16777214;
+
+        DateUtc result1 = new DateUtc(dateUtc);
+        assertEquals(dateUtc, result1.getDate());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[3];
+        data[ 0] = (byte) DateUtc.DATE_UTC_IS_NOT_KNOWN;
+        data[ 1] = (byte) (DateUtc.DATE_UTC_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) (DateUtc.DATE_UTC_IS_NOT_KNOWN >> 16);
+        //@formatter:on
+
+        DateUtc result1 = new DateUtc(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[3];
+        data[ 0] = (byte) 16777214;
+        data[ 1] = (byte) (16777214 >> 8);
+        data[ 2] = (byte) (16777214 >> 16);
+        //@formatter:on
+
+        DateUtc result1 = new DateUtc(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

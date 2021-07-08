ab9bbfe943f251f0261561862da212ee58b5cfa0
im97mori <github@im97mori.org>
2021-07-08 15:17:58 +0900

    update Electric Current Specification characteristic

--- characteristic/u2af0/src/main/java/org/im97mori/ble/characteristic/u2af0/ElectricCurrentSpecification.java
+++ characteristic/u2af0/src/main/java/org/im97mori/ble/characteristic/u2af0/ElectricCurrentSpecification.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2af0;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,22 @@ import androidx.annotation.NonNull;
 /**
  * Electric Current Specification (Characteristics UUID: 0x2AF0)
  */
-// TODO
 public class ElectricCurrentSpecification implements ByteArrayInterface {
+
+    /**
+     * Minimum Electric Current Value
+     */
+    private final int mMinimumElectricCurrentValue;
+
+    /**
+     * Typical Electric Current Value
+     */
+    private final int mTypicalElectricCurrentValue;
+
+    /**
+     * Maximum Electric Current Value
+     */
+    private final int mMaximumElectricCurrentValue;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +34,43 @@ public class ElectricCurrentSpecification implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AF0
      */
     public ElectricCurrentSpecification(@NonNull byte[] values) {
+        mMinimumElectricCurrentValue = BLEUtils.createUInt16(values, 0);
+        mTypicalElectricCurrentValue = BLEUtils.createUInt16(values, 2);
+        mMaximumElectricCurrentValue = BLEUtils.createUInt16(values, 4);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param minimumElectricCurrentValue Minimum Electric Current Value
+     * @param typicalElectricCurrentValue Typical Electric Current Value
+     * @param maximumElectricCurrentValue Maximum Electric Current Value
+     */
+    public ElectricCurrentSpecification(int minimumElectricCurrentValue, int typicalElectricCurrentValue, int maximumElectricCurrentValue) {
+        mMinimumElectricCurrentValue = minimumElectricCurrentValue;
+        mTypicalElectricCurrentValue = typicalElectricCurrentValue;
+        mMaximumElectricCurrentValue = maximumElectricCurrentValue;
+    }
+
+    /**
+     * @return Minimum Electric Current Value
+     */
+    public int getMinimumElectricCurrentValue() {
+        return mMinimumElectricCurrentValue;
+    }
+
+    /**
+     * @return Typical Electric Current Value
+     */
+    public int getTypicalElectricCurrentValue() {
+        return mTypicalElectricCurrentValue;
+    }
+
+    /**
+     * @return Maximum Electric Current Value
+     */
+    public int getMaximumElectricCurrentValue() {
+        return mMaximumElectricCurrentValue;
     }
 
     /**
@@ -24,7 +79,11 @@ public class ElectricCurrentSpecification implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[6];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.putShort((short) mMinimumElectricCurrentValue);
+        byteBuffer.putShort((short) mTypicalElectricCurrentValue);
+        byteBuffer.putShort((short) mMaximumElectricCurrentValue);
         return data;
     }
 
--- characteristic/u2af0/src/test/java/org/im97mori/ble/characteristic/u2af0/ElectricCurrentSpecificationTest.java
+++ characteristic/u2af0/src/test/java/org/im97mori/ble/characteristic/u2af0/ElectricCurrentSpecificationTest.java
@@ -1,4 +1,4 @@
-package org.im97mori.ble.characteristic.u2aef;
+package org.im97mori.ble.characteristic.u2af0;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -7,122 +7,143 @@ import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.characteristic.core.ElectricCurrentUtils;
 import org.junit.Test;
 
-public class ElectricCurrentRangeTest {
+public class ElectricCurrentSpecificationTest {
 
     @Test
     public void test_constructor_00001() {
         //@formatter:off
-        byte[] data = new byte[4];
+        byte[] data = new byte[6];
         data[ 0] = (byte) ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
         data[ 1] = (byte) (ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN >> 8);
         data[ 2] = (byte) ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
         data[ 3] = (byte) (ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 4] = (byte) ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
+        data[ 5] = (byte) (ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN >> 8);
         //@formatter:on
 
-        ElectricCurrentRange result = new ElectricCurrentRange(data);
+        ElectricCurrentSpecification result = new ElectricCurrentSpecification(data);
         assertEquals(BLEUtils.createUInt16(data, 0), result.getMinimumElectricCurrentValue());
-        assertEquals(BLEUtils.createUInt16(data, 2), result.getMaximumElectricCurrentValue());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getTypicalElectricCurrentValue());
+        assertEquals(BLEUtils.createUInt16(data, 4), result.getMaximumElectricCurrentValue());
     }
 
     @Test
     public void test_constructor_00002() {
         //@formatter:off
-        byte[] data = new byte[4];
+        byte[] data = new byte[6];
         data[ 0] = (byte) 0;
         data[ 1] = (byte) (0 >> 8);
         data[ 2] = (byte) 0;
         data[ 3] = (byte) (0 >> 8);
+        data[ 4] = (byte) 0;
+        data[ 5] = (byte) (0 >> 8);
         //@formatter:on
 
-        ElectricCurrentRange result = new ElectricCurrentRange(data);
+        ElectricCurrentSpecification result = new ElectricCurrentSpecification(data);
         assertEquals(BLEUtils.createUInt16(data, 0), result.getMinimumElectricCurrentValue());
-        assertEquals(BLEUtils.createUInt16(data, 2), result.getMaximumElectricCurrentValue());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getTypicalElectricCurrentValue());
+        assertEquals(BLEUtils.createUInt16(data, 4), result.getMaximumElectricCurrentValue());
     }
 
     @Test
     public void test_constructor_00003() {
         //@formatter:off
-        byte[] data = new byte[4];
+        byte[] data = new byte[6];
         data[ 0] = (byte) 65534;
         data[ 1] = (byte) (65534 >> 8);
         data[ 2] = (byte) 65533;
         data[ 3] = (byte) (65533 >> 8);
+        data[ 4] = (byte) 65532;
+        data[ 5] = (byte) (65532 >> 8);
         //@formatter:on
 
-        ElectricCurrentRange result = new ElectricCurrentRange(data);
+        ElectricCurrentSpecification result = new ElectricCurrentSpecification(data);
         assertEquals(BLEUtils.createUInt16(data, 0), result.getMinimumElectricCurrentValue());
-        assertEquals(BLEUtils.createUInt16(data, 2), result.getMaximumElectricCurrentValue());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getTypicalElectricCurrentValue());
+        assertEquals(BLEUtils.createUInt16(data, 4), result.getMaximumElectricCurrentValue());
     }
 
     @Test
     public void test_constructor_00101() {
         int minimumElectricCurrentValue = ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
+        int typicalElectricCurrentValue = ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
         int maximumElectricCurrentValue = ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
 
-        ElectricCurrentRange result = new ElectricCurrentRange(minimumElectricCurrentValue, maximumElectricCurrentValue);
+        ElectricCurrentSpecification result = new ElectricCurrentSpecification(minimumElectricCurrentValue, typicalElectricCurrentValue, maximumElectricCurrentValue);
         assertEquals(minimumElectricCurrentValue, result.getMinimumElectricCurrentValue());
+        assertEquals(typicalElectricCurrentValue, result.getTypicalElectricCurrentValue());
         assertEquals(maximumElectricCurrentValue, result.getMaximumElectricCurrentValue());
     }
 
     @Test
     public void test_constructor_00102() {
         int minimumElectricCurrentValue = 0;
+        int typicalElectricCurrentValue = 0;
         int maximumElectricCurrentValue = 0;
 
-        ElectricCurrentRange result = new ElectricCurrentRange(minimumElectricCurrentValue, maximumElectricCurrentValue);
+        ElectricCurrentSpecification result = new ElectricCurrentSpecification(minimumElectricCurrentValue, typicalElectricCurrentValue, maximumElectricCurrentValue);
         assertEquals(minimumElectricCurrentValue, result.getMinimumElectricCurrentValue());
+        assertEquals(typicalElectricCurrentValue, result.getTypicalElectricCurrentValue());
         assertEquals(maximumElectricCurrentValue, result.getMaximumElectricCurrentValue());
     }
 
     @Test
     public void test_constructor_00103() {
         int minimumElectricCurrentValue = 65534;
-        int maximumElectricCurrentValue = 65533;
+        int typicalElectricCurrentValue = 65533;
+        int maximumElectricCurrentValue = 65532;
 
-        ElectricCurrentRange result = new ElectricCurrentRange(minimumElectricCurrentValue, maximumElectricCurrentValue);
+        ElectricCurrentSpecification result = new ElectricCurrentSpecification(minimumElectricCurrentValue, typicalElectricCurrentValue, maximumElectricCurrentValue);
         assertEquals(minimumElectricCurrentValue, result.getMinimumElectricCurrentValue());
+        assertEquals(typicalElectricCurrentValue, result.getTypicalElectricCurrentValue());
         assertEquals(maximumElectricCurrentValue, result.getMaximumElectricCurrentValue());
     }
 
     @Test
     public void test_parcelable_00101() {
         //@formatter:off
-        byte[] data = new byte[4];
+        byte[] data = new byte[6];
         data[ 0] = (byte) ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
         data[ 1] = (byte) (ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN >> 8);
         data[ 2] = (byte) ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
         data[ 3] = (byte) (ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 4] = (byte) ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN;
+        data[ 5] = (byte) (ElectricCurrentUtils.CURRENT_VALUE_IS_NOT_KNOWN >> 8);
         //@formatter:on
 
-        ElectricCurrentRange result1 = new ElectricCurrentRange(data);
+        ElectricCurrentSpecification result1 = new ElectricCurrentSpecification(data);
         assertArrayEquals(data, result1.getBytes());
     }
 
     @Test
     public void test_parcelable_00102() {
         //@formatter:off
-        byte[] data = new byte[4];
+        byte[] data = new byte[6];
         data[ 0] = (byte) 0;
         data[ 1] = (byte) (0 >> 8);
         data[ 2] = (byte) 0;
         data[ 3] = (byte) (0 >> 8);
+        data[ 4] = (byte) 0;
+        data[ 5] = (byte) (0 >> 8);
         //@formatter:on
 
-        ElectricCurrentRange result1 = new ElectricCurrentRange(data);
+        ElectricCurrentSpecification result1 = new ElectricCurrentSpecification(data);
         assertArrayEquals(data, result1.getBytes());
     }
 
     @Test
     public void test_parcelable_00103() {
         //@formatter:off
-        byte[] data = new byte[4];
+        byte[] data = new byte[6];
         data[ 0] = (byte) 65534;
         data[ 1] = (byte) (65534 >> 8);
         data[ 2] = (byte) 65533;
         data[ 3] = (byte) (65533 >> 8);
+        data[ 4] = (byte) 65532;
+        data[ 5] = (byte) (65532 >> 8);
         //@formatter:on
 
-        ElectricCurrentRange result1 = new ElectricCurrentRange(data);
+        ElectricCurrentSpecification result1 = new ElectricCurrentSpecification(data);
         assertArrayEquals(data, result1.getBytes());
     }
 

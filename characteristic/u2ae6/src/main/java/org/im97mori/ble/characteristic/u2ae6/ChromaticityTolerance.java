5be4115bce1b15b718072f0443032a9875dbbab4
im97mori <github@im97mori.org>
2021-07-10 15:20:09 +0900

    update Chromaticity Tolerance characteristic

--- characteristic/u2ae6/src/main/java/org/im97mori/ble/characteristic/u2ae6/ChromaticityTolerance.java
+++ characteristic/u2ae6/src/main/java/org/im97mori/ble/characteristic/u2ae6/ChromaticityTolerance.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2ae6;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,27 @@ import androidx.annotation.NonNull;
 /**
  * Chromaticity Tolerance (Characteristics UUID: 0x2AE6)
  */
-// TODO
 public class ChromaticityTolerance implements ByteArrayInterface {
+
+    /**
+     * Unit is volt with a resolution of 1/64V
+     */
+    public static final double CHROMATICITY_VALUE_UNIT = 0.0001d;
+
+    /**
+     * Chromaticity Tolerance Minimum value
+     */
+    public static final double CHROMATICITY_VALUE_MINIMUM = 0.0d;
+
+    /**
+     * Chromaticity Tolerance Maximum value
+     */
+    public static final double CHROMATICITY_VALUE_MAXIMUM = 0.0255d;
+
+    /**
+     * Chromaticity Tolerance
+     */
+    private final int mChromaticityTolerance;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +39,30 @@ public class ChromaticityTolerance implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AE6
      */
     public ChromaticityTolerance(@NonNull byte[] values) {
+        mChromaticityTolerance = BLEUtils.createUInt8(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param chromaticityTolerance Chromaticity Tolerance
+     */
+    public ChromaticityTolerance(int chromaticityTolerance) {
+        mChromaticityTolerance = chromaticityTolerance;
+    }
+
+    /**
+     * @return Chromaticity Tolerance
+     */
+    public int getChromaticityTolerance() {
+        return mChromaticityTolerance;
+    }
+
+    /**
+     * @return Chromaticity Tolerance with Unit
+     */
+    public double getChromaticityToleranceWithUnit() {
+        return CHROMATICITY_VALUE_UNIT * mChromaticityTolerance;
     }
 
     /**
@@ -24,7 +71,9 @@ public class ChromaticityTolerance implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[1];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mChromaticityTolerance);
         return data;
     }
 
---
+++ characteristic/u2ae6/src/test/java/org/im97mori/ble/characteristic/u2ae6/ChromaticityToleranceTest.java
@@ -0,0 +1,75 @@
+package org.im97mori.ble.characteristic.u2ae6;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.im97mori.ble.BLEUtils;
+import org.junit.Test;
+
+public class ChromaticityToleranceTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = 0x00;
+        //@formatter:on
+
+        ChromaticityTolerance result = new ChromaticityTolerance(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getChromaticityTolerance());
+        assertEquals(BLEUtils.createUInt8(data, 0) * ChromaticityTolerance.CHROMATICITY_VALUE_UNIT, result.getChromaticityToleranceWithUnit(), 0);
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) 0xff;
+        //@formatter:on
+
+        ChromaticityTolerance result = new ChromaticityTolerance(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getChromaticityTolerance());
+        assertEquals(BLEUtils.createUInt8(data, 0) * ChromaticityTolerance.CHROMATICITY_VALUE_UNIT, result.getChromaticityToleranceWithUnit(), 0);
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int chromaticityTolerance = 0;
+
+        ChromaticityTolerance result = new ChromaticityTolerance(chromaticityTolerance);
+        assertEquals(chromaticityTolerance, result.getChromaticityTolerance());
+        assertEquals(chromaticityTolerance * ChromaticityTolerance.CHROMATICITY_VALUE_UNIT, result.getChromaticityToleranceWithUnit(), 0);
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int chromaticityTolerance = 255;
+
+        ChromaticityTolerance result = new ChromaticityTolerance(chromaticityTolerance);
+        assertEquals(chromaticityTolerance, result.getChromaticityTolerance());
+        assertEquals(chromaticityTolerance * ChromaticityTolerance.CHROMATICITY_VALUE_UNIT, result.getChromaticityToleranceWithUnit(), 0);
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = 0x01;
+        //@formatter:on
+
+        ChromaticityTolerance result1 = new ChromaticityTolerance(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+    
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) 0xff;
+        //@formatter:on
+
+        ChromaticityTolerance result1 = new ChromaticityTolerance(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

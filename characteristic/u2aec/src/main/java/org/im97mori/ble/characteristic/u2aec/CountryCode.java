05a3e5dcb963abcf7a9e63685d6807c83106f94a
im97mori <github@im97mori.org>
2021-07-14 16:27:51 +0900

    update Country Code characteristic

--- characteristic/u2aec/src/main/java/org/im97mori/ble/characteristic/u2aec/CountryCode.java
+++ characteristic/u2aec/src/main/java/org/im97mori/ble/characteristic/u2aec/CountryCode.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2aec;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,27 @@ import androidx.annotation.NonNull;
 /**
  * Country Code (Characteristics UUID: 0x2AEC)
  */
-// TODO
 public class CountryCode implements ByteArrayInterface {
+
+    /**
+     * 0xFFFF : A value of 0xFFFF represents ’value is not known’
+     */
+    public static final int COUNTRY_CODE_VALUE_IS_NOT_KNOWN = 0xFFFF;
+
+    /**
+     * Country Code Minimum value
+     */
+    public static final int COUNTRY_CODE_VALUE_MINIMUM = 0;
+
+    /**
+     * Country Code Maximum value
+     */
+    public static final int COUNTRY_CODE_VALUE_MAXIMUM = 4095;
+
+    /**
+     * Country Code
+     */
+    private final int mCountryCode;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +39,31 @@ public class CountryCode implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AEC
      */
     public CountryCode(@NonNull byte[] values) {
+        mCountryCode = BLEUtils.createUInt16(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param countryCode Country Code
+     */
+    public CountryCode(int countryCode) {
+        mCountryCode = countryCode;
+    }
+
+    /**
+     * @return Country Code
+     */
+    public int getCountryCode() {
+        return mCountryCode;
+    }
+
+    /**
+     * @return {@code true}:Country Code value is not known, {@code false}:has Country Code value information
+     * @see #COUNTRY_CODE_VALUE_IS_NOT_KNOWN
+     */
+    public boolean isCountryCodeValueIsNotKnown() {
+        return COUNTRY_CODE_VALUE_IS_NOT_KNOWN == mCountryCode;
     }
 
     /**
@@ -24,7 +72,9 @@ public class CountryCode implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[2];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.putShort((short) mCountryCode);
         return data;
     }
 
---
+++ characteristic/u2aec/src/test/java/org/im97mori/ble/characteristic/u2aec/CountryCodeTest.java
@@ -0,0 +1,115 @@
+package org.im97mori.ble.characteristic.u2aec;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.im97mori.ble.BLEUtils;
+import org.junit.Test;
+
+public class CountryCodeTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) CountryCode.COUNTRY_CODE_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (CountryCode.COUNTRY_CODE_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        CountryCode result = new CountryCode(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getCountryCode());
+        assertTrue(result.isCountryCodeValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) CountryCode.COUNTRY_CODE_VALUE_MINIMUM;
+        data[ 1] = (byte) (CountryCode.COUNTRY_CODE_VALUE_MINIMUM >> 8);
+        //@formatter:on
+
+        CountryCode result = new CountryCode(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getCountryCode());
+        assertFalse(result.isCountryCodeValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) CountryCode.COUNTRY_CODE_VALUE_MAXIMUM;
+        data[ 1] = (byte) (CountryCode.COUNTRY_CODE_VALUE_MAXIMUM >> 8);
+        //@formatter:on
+
+        CountryCode result = new CountryCode(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getCountryCode());
+        assertFalse(result.isCountryCodeValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int countryCode = CountryCode.COUNTRY_CODE_VALUE_IS_NOT_KNOWN;
+
+        CountryCode result = new CountryCode(countryCode);
+        assertEquals(countryCode, result.getCountryCode());
+        assertTrue(result.isCountryCodeValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int countryCode = CountryCode.COUNTRY_CODE_VALUE_MINIMUM;
+
+        CountryCode result = new CountryCode(countryCode);
+        assertEquals(countryCode, result.getCountryCode());
+        assertFalse(result.isCountryCodeValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int countryCode = CountryCode.COUNTRY_CODE_VALUE_MAXIMUM;
+
+        CountryCode result = new CountryCode(countryCode);
+        assertEquals(countryCode, result.getCountryCode());
+        assertFalse(result.isCountryCodeValueIsNotKnown());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) CountryCode.COUNTRY_CODE_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (CountryCode.COUNTRY_CODE_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        CountryCode result1 = new CountryCode(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) CountryCode.COUNTRY_CODE_VALUE_MINIMUM;
+        data[ 1] = (byte) (CountryCode.COUNTRY_CODE_VALUE_MINIMUM >> 8);
+        //@formatter:on
+
+        CountryCode result1 = new CountryCode(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) CountryCode.COUNTRY_CODE_VALUE_MAXIMUM;
+        data[ 1] = (byte) (CountryCode.COUNTRY_CODE_VALUE_MAXIMUM >> 8);
+        //@formatter:on
+
+        CountryCode result1 = new CountryCode(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

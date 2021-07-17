2683ce3d302c32d6c739646d836d502a67f2669d
im97mori <github@im97mori.org>
2021-07-17 16:05:58 +0900

    update Time Second 8 characteristic

---
+++ characteristic/u2b17/src/main/java/org/im97mori/ble/characteristic/u2b17/TimeSecond8.java
@@ -0,0 +1,81 @@
+package org.im97mori.ble.characteristic.u2b17;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.ByteArrayInterface;
+
+import androidx.annotation.NonNull;
+
+/**
+ * Time Second 8 (Characteristics UUID: 0x2B17)
+ */
+public class TimeSecond8 implements ByteArrayInterface {
+
+    /**
+     * 0xFF : A value of 0xFF represents ‘value is not known’
+     */
+    public static final int TIME_SECOND_8_VALUE_IS_NOT_KNOWN = 0xFF;
+
+    /**
+     * Time Second 8 Minimum value
+     */
+    public static final int TIME_SECOND_8_VALUE_MINIMUM = 0;
+
+    /**
+     * Time Second 8 Maximum value
+     */
+    public static final int TIME_SECOND_8_VALUE_MAXIMUM = 254;
+
+    /**
+     * Time Second 8
+     */
+    private final int mTimeSecond8;
+
+    /**
+     * Constructor from {@link BluetoothGattCharacteristic}
+     *
+     * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B17
+     */
+    public TimeSecond8(@NonNull byte[] values) {
+        mTimeSecond8 = BLEUtils.createUInt8(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param timeSecond8 Time Second 8
+     */
+    public TimeSecond8(int timeSecond8) {
+        mTimeSecond8 = timeSecond8;
+    }
+
+    /**
+     * @return Time Second 8
+     */
+    public int getTimeSecond8() {
+        return mTimeSecond8;
+    }
+
+    /**
+     * @return {@code true}:Time Second 8 value is not known, {@code false}:has Time Second 8 value information
+     * @see #TIME_SECOND_8_VALUE_IS_NOT_KNOWN
+     */
+    public boolean isTimeSecond8ValueIsNotKnown() {
+        return TIME_SECOND_8_VALUE_IS_NOT_KNOWN == mTimeSecond8;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @NonNull
+    public byte[] getBytes() {
+        byte[] data = new byte[1];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mTimeSecond8);
+        return data;
+    }
+
+}
---
+++ characteristic/u2b17/src/test/java/org/im97mori/ble/characteristic/u2b17/TimeSecond8Test.java
@@ -0,0 +1,109 @@
+package org.im97mori.ble.characteristic.u2b17;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.im97mori.ble.BLEUtils;
+import org.junit.Test;
+
+public class TimeSecond8Test {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSecond8.TIME_SECOND_8_VALUE_IS_NOT_KNOWN;
+        //@formatter:on
+
+        TimeSecond8 result = new TimeSecond8(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getTimeSecond8());
+        assertTrue(result.isTimeSecond8ValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSecond8.TIME_SECOND_8_VALUE_MINIMUM;
+        //@formatter:on
+
+        TimeSecond8 result = new TimeSecond8(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getTimeSecond8());
+        assertFalse(result.isTimeSecond8ValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSecond8.TIME_SECOND_8_VALUE_MAXIMUM;
+        //@formatter:on
+
+        TimeSecond8 result = new TimeSecond8(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getTimeSecond8());
+        assertFalse(result.isTimeSecond8ValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int energy = TimeSecond8.TIME_SECOND_8_VALUE_IS_NOT_KNOWN;
+
+        TimeSecond8 result = new TimeSecond8(energy);
+        assertEquals(energy, result.getTimeSecond8());
+        assertTrue(result.isTimeSecond8ValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int energy = TimeSecond8.TIME_SECOND_8_VALUE_MINIMUM;
+
+        TimeSecond8 result = new TimeSecond8(energy);
+        assertEquals(energy, result.getTimeSecond8());
+        assertFalse(result.isTimeSecond8ValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int energy = TimeSecond8.TIME_SECOND_8_VALUE_MAXIMUM;
+
+        TimeSecond8 result = new TimeSecond8(energy);
+        assertEquals(energy, result.getTimeSecond8());
+        assertFalse(result.isTimeSecond8ValueIsNotKnown());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSecond8.TIME_SECOND_8_VALUE_IS_NOT_KNOWN;
+        //@formatter:on
+
+        TimeSecond8 result1 = new TimeSecond8(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSecond8.TIME_SECOND_8_VALUE_MINIMUM;
+        //@formatter:on
+
+        TimeSecond8 result1 = new TimeSecond8(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[1];
+        data[ 0] = (byte) TimeSecond8.TIME_SECOND_8_VALUE_MAXIMUM;
+        //@formatter:on
+
+        TimeSecond8 result1 = new TimeSecond8(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

cdcf2ce5734e0f5913a29018cb86c612e26b0cf7
im97mori <github@im97mori.org>
2021-07-18 14:17:17 +0900

    update Event Statistics characteristic

--- characteristic/u2af4/src/main/java/org/im97mori/ble/characteristic/u2af4/EventStatistics.java
+++ characteristic/u2af4/src/main/java/org/im97mori/ble/characteristic/u2af4/EventStatistics.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2af4;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,27 @@ import androidx.annotation.NonNull;
 /**
  * Event Statistics (Characteristics UUID: 0x2AF4)
  */
-// TODO
 public class EventStatistics implements ByteArrayInterface {
+
+    /**
+     * Number of Events
+     */
+    private final int mNumberOfEvents;
+
+    /**
+     * Average Event Duration
+     */
+    private final int mAverageEventDuration;
+
+    /**
+     * Time Elapsed Since Last Event
+     */
+    private final int mTimeElapsedSinceLastEvent;
+
+    /**
+     * Sensing Duration
+     */
+    private final int mSensingDuration;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +39,53 @@ public class EventStatistics implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AF4
      */
     public EventStatistics(@NonNull byte[] values) {
+        mNumberOfEvents = BLEUtils.createUInt16(values, 0);
+        mAverageEventDuration = BLEUtils.createUInt16(values, 2);
+        mTimeElapsedSinceLastEvent = BLEUtils.createUInt8(values, 4);
+        mSensingDuration = BLEUtils.createUInt8(values, 5);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param numberOfEvents Number of Events
+     * @param averageEventDuration Average Event Duration
+     * @param timeElapsedSinceLastEvent Time Elapsed Since Last Event
+     * @param sensingDuration Sensing Duration
+     */
+    public EventStatistics(int numberOfEvents, int averageEventDuration, int timeElapsedSinceLastEvent, int sensingDuration) {
+        mNumberOfEvents = numberOfEvents;
+        mAverageEventDuration = averageEventDuration;
+        mTimeElapsedSinceLastEvent = timeElapsedSinceLastEvent;
+        mSensingDuration = sensingDuration;
+    }
+
+    /**
+     * @return Number of Events
+     */
+    public int getNumberOfEvents() {
+        return mNumberOfEvents;
+    }
+
+    /**
+     * @return Average Event Duration
+     */
+    public int getAverageEventDuration() {
+        return mAverageEventDuration;
+    }
+
+    /**
+     * @return Sensing Duration
+     */
+    public int getTimeElapsedSinceLastEvent() {
+        return mTimeElapsedSinceLastEvent;
+    }
+
+    /**
+     * @return Time Elapsed Since Last Event
+     */
+    public int getSensingDuration() {
+        return mSensingDuration;
     }
 
     /**
@@ -24,7 +94,12 @@ public class EventStatistics implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[6];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.putShort((short) mNumberOfEvents);
+        byteBuffer.putShort((short) mAverageEventDuration);
+        byteBuffer.put((byte) mTimeElapsedSinceLastEvent);
+        byteBuffer.put((byte) mSensingDuration);
         return data;
     }
 
---
+++ characteristic/u2af4/src/test/java/org/im97mori/ble/characteristic/u2af4/EventStatisticsTest.java
@@ -0,0 +1,210 @@
+package org.im97mori.ble.characteristic.u2af4;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.characteristic.core.Count16Utils;
+import org.im97mori.ble.characteristic.core.TimeExponential8Utils;
+import org.im97mori.ble.characteristic.core.TimeSecond16Utils;
+import org.junit.Test;
+
+public class EventStatisticsTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) Count16Utils.COUNT_IS_NOT_KNOWN;
+        data[ 1] = (byte) (Count16Utils.COUNT_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) TimeSecond16Utils.TIME_SECOND_16_VALUE_IS_NOT_KNOWN;
+        data[ 3] = (byte) (TimeSecond16Utils.TIME_SECOND_16_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 4] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_VALUE_IS_NOT_KNOWN;
+        data[ 5] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_VALUE_IS_NOT_KNOWN;
+        //@formatter:on
+
+        EventStatistics result = new EventStatistics(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getNumberOfEvents());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getAverageEventDuration());
+        assertEquals(BLEUtils.createUInt8(data, 4), result.getTimeElapsedSinceLastEvent());
+        assertEquals(BLEUtils.createUInt8(data, 5), result.getSensingDuration());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) Count16Utils.COUNT_VALUE_MINIMUM;
+        data[ 1] = (byte) (Count16Utils.COUNT_VALUE_MINIMUM >> 8);
+        data[ 2] = (byte) TimeSecond16Utils.TIME_SECOND_16_VALUE_MINIMUM;
+        data[ 3] = (byte) (TimeSecond16Utils.TIME_SECOND_16_VALUE_MINIMUM >> 8);
+        data[ 4] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_TOTAL_LIFE_OF_THE_DEVICE;
+        data[ 5] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_TOTAL_LIFE_OF_THE_DEVICE;
+        //@formatter:on
+
+        EventStatistics result = new EventStatistics(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getNumberOfEvents());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getAverageEventDuration());
+        assertEquals(BLEUtils.createUInt8(data, 4), result.getTimeElapsedSinceLastEvent());
+        assertEquals(BLEUtils.createUInt8(data, 5), result.getSensingDuration());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) Count16Utils.COUNT_VALUE_MAXIMUM;
+        data[ 1] = (byte) (Count16Utils.COUNT_VALUE_MAXIMUM >> 8);
+        data[ 2] = (byte) TimeSecond16Utils.TIME_SECOND_16_VALUE_MAXIMUM;
+        data[ 3] = (byte) (TimeSecond16Utils.TIME_SECOND_16_VALUE_MAXIMUM >> 8);
+        data[ 4] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_ZERO_SECONDS;
+        data[ 5] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_ZERO_SECONDS;
+        //@formatter:on
+
+        EventStatistics result = new EventStatistics(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getNumberOfEvents());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getAverageEventDuration());
+        assertEquals(BLEUtils.createUInt8(data, 4), result.getTimeElapsedSinceLastEvent());
+        assertEquals(BLEUtils.createUInt8(data, 5), result.getSensingDuration());
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        data[ 5] = 0x06;
+        //@formatter:on
+
+        EventStatistics result = new EventStatistics(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getNumberOfEvents());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getAverageEventDuration());
+        assertEquals(BLEUtils.createUInt8(data, 4), result.getTimeElapsedSinceLastEvent());
+        assertEquals(BLEUtils.createUInt8(data, 5), result.getSensingDuration());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int numberOfEvents = Count16Utils.COUNT_IS_NOT_KNOWN;
+        int averageEventDuration = TimeSecond16Utils.TIME_SECOND_16_VALUE_IS_NOT_KNOWN;
+        int timeElapsedSinceLastEvent = TimeExponential8Utils.TIME_EXPONENTIAL_8_VALUE_IS_NOT_KNOWN;
+        int sensingDuration = TimeExponential8Utils.TIME_EXPONENTIAL_8_VALUE_IS_NOT_KNOWN;
+
+        EventStatistics result = new EventStatistics(numberOfEvents, averageEventDuration, timeElapsedSinceLastEvent, sensingDuration);
+        assertEquals(numberOfEvents, result.getNumberOfEvents());
+        assertEquals(averageEventDuration, result.getAverageEventDuration());
+        assertEquals(timeElapsedSinceLastEvent, result.getTimeElapsedSinceLastEvent());
+        assertEquals(sensingDuration, result.getSensingDuration());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int numberOfEvents = Count16Utils.COUNT_VALUE_MINIMUM;
+        int averageEventDuration = TimeSecond16Utils.TIME_SECOND_16_VALUE_MINIMUM;
+        int timeElapsedSinceLastEvent = TimeExponential8Utils.TIME_EXPONENTIAL_8_TOTAL_LIFE_OF_THE_DEVICE;
+        int sensingDuration = TimeExponential8Utils.TIME_EXPONENTIAL_8_TOTAL_LIFE_OF_THE_DEVICE;
+
+        EventStatistics result = new EventStatistics(numberOfEvents, averageEventDuration, timeElapsedSinceLastEvent, sensingDuration);
+        assertEquals(numberOfEvents, result.getNumberOfEvents());
+        assertEquals(averageEventDuration, result.getAverageEventDuration());
+        assertEquals(timeElapsedSinceLastEvent, result.getTimeElapsedSinceLastEvent());
+        assertEquals(sensingDuration, result.getSensingDuration());
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int numberOfEvents = Count16Utils.COUNT_VALUE_MAXIMUM;
+        int averageEventDuration = TimeSecond16Utils.TIME_SECOND_16_VALUE_MAXIMUM;
+        int timeElapsedSinceLastEvent = TimeExponential8Utils.TIME_EXPONENTIAL_8_ZERO_SECONDS;
+        int sensingDuration = TimeExponential8Utils.TIME_EXPONENTIAL_8_ZERO_SECONDS;
+
+        EventStatistics result = new EventStatistics(numberOfEvents, averageEventDuration, timeElapsedSinceLastEvent, sensingDuration);
+        assertEquals(numberOfEvents, result.getNumberOfEvents());
+        assertEquals(averageEventDuration, result.getAverageEventDuration());
+        assertEquals(timeElapsedSinceLastEvent, result.getTimeElapsedSinceLastEvent());
+        assertEquals(sensingDuration, result.getSensingDuration());
+    }
+
+    @Test
+    public void test_constructor_00104() {
+        int numberOfEvents = 1;
+        int averageEventDuration = 2;
+        int timeElapsedSinceLastEvent = 3;
+        int sensingDuration = 4;
+
+        EventStatistics result = new EventStatistics(numberOfEvents, averageEventDuration, timeElapsedSinceLastEvent, sensingDuration);
+        assertEquals(numberOfEvents, result.getNumberOfEvents());
+        assertEquals(averageEventDuration, result.getAverageEventDuration());
+        assertEquals(timeElapsedSinceLastEvent, result.getTimeElapsedSinceLastEvent());
+        assertEquals(sensingDuration, result.getSensingDuration());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) Count16Utils.COUNT_IS_NOT_KNOWN;
+        data[ 1] = (byte) (Count16Utils.COUNT_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) TimeSecond16Utils.TIME_SECOND_16_VALUE_IS_NOT_KNOWN;
+        data[ 3] = (byte) (TimeSecond16Utils.TIME_SECOND_16_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 4] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_VALUE_IS_NOT_KNOWN;
+        data[ 5] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_VALUE_IS_NOT_KNOWN;
+        //@formatter:on
+
+        EventStatistics result1 = new EventStatistics(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) Count16Utils.COUNT_VALUE_MINIMUM;
+        data[ 1] = (byte) (Count16Utils.COUNT_VALUE_MINIMUM >> 8);
+        data[ 2] = (byte) TimeSecond16Utils.TIME_SECOND_16_VALUE_MINIMUM;
+        data[ 3] = (byte) (TimeSecond16Utils.TIME_SECOND_16_VALUE_MINIMUM >> 8);
+        data[ 4] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_TOTAL_LIFE_OF_THE_DEVICE;
+        data[ 5] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_TOTAL_LIFE_OF_THE_DEVICE;
+        //@formatter:on
+
+        EventStatistics result1 = new EventStatistics(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) Count16Utils.COUNT_VALUE_MAXIMUM;
+        data[ 1] = (byte) (Count16Utils.COUNT_VALUE_MAXIMUM >> 8);
+        data[ 2] = (byte) TimeSecond16Utils.TIME_SECOND_16_VALUE_MAXIMUM;
+        data[ 3] = (byte) (TimeSecond16Utils.TIME_SECOND_16_VALUE_MAXIMUM >> 8);
+        data[ 4] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_ZERO_SECONDS;
+        data[ 5] = (byte) TimeExponential8Utils.TIME_EXPONENTIAL_8_ZERO_SECONDS;
+        //@formatter:on
+
+        EventStatistics result1 = new EventStatistics(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00104() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        data[ 5] = 0x06;
+        //@formatter:on
+
+        EventStatistics result1 = new EventStatistics(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

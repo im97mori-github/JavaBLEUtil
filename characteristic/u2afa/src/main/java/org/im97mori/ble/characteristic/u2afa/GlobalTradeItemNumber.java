94cb927395832bc5ef75686c0c2f26a94176089f
im97mori <github@im97mori.org>
2021-07-19 18:23:04 +0900

    update Global Trade Item Number characteristic

--- characteristic/u2afa/src/main/java/org/im97mori/ble/characteristic/u2afa/GlobalTradeItemNumber.java
+++ characteristic/u2afa/src/main/java/org/im97mori/ble/characteristic/u2afa/GlobalTradeItemNumber.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2afa;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,12 @@ import androidx.annotation.NonNull;
 /**
  * Global Trade Item Number (Characteristics UUID: 0x2AFA)
  */
-// TODO
 public class GlobalTradeItemNumber implements ByteArrayInterface {
+
+    /**
+     * Global Trade Item Number
+     */
+    private final long mGlobalTradeItemNumber;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +24,37 @@ public class GlobalTradeItemNumber implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AFA
      */
     public GlobalTradeItemNumber(@NonNull byte[] values) {
+        mGlobalTradeItemNumber = BLEUtils.createUInt48(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param globalTradeItemNumber Global Trade Item Number
+     */
+    public GlobalTradeItemNumber(long globalTradeItemNumber) {
+        mGlobalTradeItemNumber = globalTradeItemNumber;
+    }
+
+    /**
+     * @return Global Trade Item Number
+     */
+    public long getGlobalTradeItemNumber() {
+        return mGlobalTradeItemNumber;
+    }
+
+    /**
+     * check digit validation
+     * @return {@code true}:valid, {@code false}:not valid
+     */
+    public boolean isValid() {
+        char[] arrays = String.valueOf(mGlobalTradeItemNumber).toCharArray();
+        int length = arrays.length;
+        int total = 0;
+        for (int i = 0; i < length - 1; i++) {
+            total += Character.getNumericValue(arrays[length - 2 - i]) * (i % 2 == 0 ? 3 : 1);
+        }
+        return Character.getNumericValue(arrays[length - 1]) == 10 - total % 10;
     }
 
     /**
@@ -24,7 +63,14 @@ public class GlobalTradeItemNumber implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[6];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mGlobalTradeItemNumber);
+        byteBuffer.put((byte) (mGlobalTradeItemNumber >> 8));
+        byteBuffer.put((byte) (mGlobalTradeItemNumber >> 16));
+        byteBuffer.put((byte) (mGlobalTradeItemNumber >> 24));
+        byteBuffer.put((byte) (mGlobalTradeItemNumber >> 32));
+        byteBuffer.put((byte) (mGlobalTradeItemNumber >> 40));
         return data;
     }
 
---
+++ characteristic/u2afa/src/test/java/org/im97mori/ble/characteristic/u2afa/GlobalTradeItemNumberTest.java
@@ -0,0 +1,105 @@
+package org.im97mori.ble.characteristic.u2afa;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.im97mori.ble.BLEUtils;
+import org.junit.Test;
+
+public class GlobalTradeItemNumberTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        data[ 5] = 0x06;
+        //@formatter:on
+
+        GlobalTradeItemNumber result = new GlobalTradeItemNumber(data);
+        assertEquals(BLEUtils.createUInt48(data, 0), result.getGlobalTradeItemNumber());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) 12345678912343L;
+        data[ 1] = (byte) (12345678912343L >> 8);
+        data[ 2] = (byte) (12345678912343L >> 16);
+        data[ 3] = (byte) (12345678912343L >> 24);
+        data[ 4] = (byte) (12345678912343L >> 32);
+        data[ 5] = (byte) (12345678912343L >> 40);
+        //@formatter:on
+
+        GlobalTradeItemNumber result = new GlobalTradeItemNumber(data);
+        assertEquals(BLEUtils.createUInt48(data, 0), result.getGlobalTradeItemNumber());
+        assertTrue(result.isValid());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) 12345678912344L;
+        data[ 1] = (byte) (12345678912344L >> 8);
+        data[ 2] = (byte) (12345678912344L >> 16);
+        data[ 3] = (byte) (12345678912344L >> 24);
+        data[ 4] = (byte) (12345678912344L >> 32);
+        data[ 5] = (byte) (12345678912344L >> 40);
+        //@formatter:on
+
+        GlobalTradeItemNumber result = new GlobalTradeItemNumber(data);
+        assertEquals(BLEUtils.createUInt48(data, 0), result.getGlobalTradeItemNumber());
+        assertFalse(result.isValid());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        long globalTradeItemNumber = 1;
+
+        GlobalTradeItemNumber result = new GlobalTradeItemNumber(globalTradeItemNumber);
+        assertEquals(globalTradeItemNumber, result.getGlobalTradeItemNumber());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        long globalTradeItemNumber = 12345678912343L;
+
+        GlobalTradeItemNumber result = new GlobalTradeItemNumber(globalTradeItemNumber);
+        assertEquals(globalTradeItemNumber, result.getGlobalTradeItemNumber());
+        assertTrue(result.isValid());
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        long globalTradeItemNumber = 12345678912344L;
+
+        GlobalTradeItemNumber result = new GlobalTradeItemNumber(globalTradeItemNumber);
+        assertEquals(globalTradeItemNumber, result.getGlobalTradeItemNumber());
+        assertFalse(result.isValid());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 1] = 0x04;
+        data[ 1] = 0x05;
+        data[ 1] = 0x06;
+        //@formatter:on
+
+        GlobalTradeItemNumber result1 = new GlobalTradeItemNumber(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

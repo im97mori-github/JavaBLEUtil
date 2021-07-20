f5132ebba77e7ea4dddbc69cd75a4f2d533e80ea
im97mori <github@im97mori.org>
2021-07-20 14:03:41 +0900

    update Luminous Efficacy characteristic

--- characteristic/u2afc/src/main/java/org/im97mori/ble/characteristic/u2afc/LuminousEfficacy.java
+++ characteristic/u2afc/src/main/java/org/im97mori/ble/characteristic/u2afc/LuminousEfficacy.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2afc;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,32 @@ import androidx.annotation.NonNull;
 /**
  * Luminous Efficacy (Characteristics UUID: 0x2AFC)
  */
-// TODO
 public class LuminousEfficacy implements ByteArrayInterface {
+
+    /**
+     * 0xFFFF : A raw value of 0xFFFF represents ‘value is not known’
+     */
+    public static final int LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN = 0xFFFF;
+
+    /**
+     * Unit is lumen per watt with a resolution of 0.01
+     */
+    public static final double LUMINOUS_EFFICACY_VALUE_UNIT = 0.1d;
+
+    /**
+     * Illuminance Minimum value
+     */
+    public static final double LUMINOUS_EFFICACY_VALUE_MINIMUM = 0d;
+
+    /**
+     * Illuminance Maximum value
+     */
+    public static final double LUMINOUS_EFFICACY_VALUE_MAXIMUM = 1800d;
+
+    /**
+     * Luminous Efficacy
+     */
+    private final int mLuminousEfficacy;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +44,44 @@ public class LuminousEfficacy implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2AFC
      */
     public LuminousEfficacy(@NonNull byte[] values) {
+        mLuminousEfficacy = BLEUtils.createUInt16(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param luminousEfficacy Luminous Efficacy
+     */
+    public LuminousEfficacy(int luminousEfficacy) {
+        mLuminousEfficacy = luminousEfficacy;
+    }
+
+    /**
+     * @return Luminous Efficacy
+     */
+    public int getLuminousEfficacy() {
+        return mLuminousEfficacy;
+    }
+
+    /**
+     * @return {@code true}:Luminous Efficacy value is not known, {@code false}:has Luminous Efficacy value information
+     * @see #LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN
+     */
+    public boolean isLuminousEfficacyValueIsNotKnown() {
+        return LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN == mLuminousEfficacy;
+    }
+
+    /**
+     * @return Luminous Efficacy(lumen per watt)
+     */
+    public double getLuminousEfficacyLumenPerWatt() {
+        double luminousEfficacyLumenPerWatt = mLuminousEfficacy * LUMINOUS_EFFICACY_VALUE_UNIT;
+        if (luminousEfficacyLumenPerWatt < LUMINOUS_EFFICACY_VALUE_MINIMUM) {
+            luminousEfficacyLumenPerWatt = LUMINOUS_EFFICACY_VALUE_MINIMUM;
+        } else if (luminousEfficacyLumenPerWatt > LUMINOUS_EFFICACY_VALUE_MAXIMUM) {
+            luminousEfficacyLumenPerWatt = LUMINOUS_EFFICACY_VALUE_MAXIMUM;
+        }
+        return luminousEfficacyLumenPerWatt;
     }
 
     /**
@@ -24,7 +90,10 @@ public class LuminousEfficacy implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[2];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mLuminousEfficacy);
+        byteBuffer.put((byte) (mLuminousEfficacy >> 8));
         return data;
     }
 
---
+++ characteristic/u2afc/src/test/java/org/im97mori/ble/characteristic/u2afc/LuminousEfficacyTest.java
@@ -0,0 +1,236 @@
+package org.im97mori.ble.characteristic.u2afc;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.im97mori.ble.BLEUtils;
+import org.junit.Test;
+
+public class LuminousEfficacyTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result = new LuminousEfficacy(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getLuminousEfficacy());
+        assertTrue(result.isLuminousEfficacyValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MINIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT);
+        data[ 1] = (byte) (((int) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MINIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT)) >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result = new LuminousEfficacy(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(BLEUtils.createUInt16(data, 0) * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MAXIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT);
+        data[ 1] = (byte) (((int) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MAXIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT)) >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result = new LuminousEfficacy(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(BLEUtils.createUInt16(data, 0) * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 18001;
+        data[ 1] = (byte) (18001 >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result = new LuminousEfficacy(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MAXIMUM, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00005() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 1;
+        data[ 1] = (byte) (1 >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result = new LuminousEfficacy(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(BLEUtils.createUInt16(data, 0) * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00006() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 17999;
+        data[ 1] = (byte) (17999 >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result = new LuminousEfficacy(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(BLEUtils.createUInt16(data, 0) * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int luminousEfficacy = LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN;
+
+        LuminousEfficacy result = new LuminousEfficacy(luminousEfficacy);
+        assertEquals(luminousEfficacy, result.getLuminousEfficacy());
+        assertTrue(result.isLuminousEfficacyValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int luminousEfficacy = (int) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MINIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT);
+
+        LuminousEfficacy result = new LuminousEfficacy(luminousEfficacy);
+        assertEquals(luminousEfficacy, result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(luminousEfficacy * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int luminousEfficacy = (int) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MAXIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT);
+
+        LuminousEfficacy result = new LuminousEfficacy(luminousEfficacy);
+        assertEquals(luminousEfficacy, result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(luminousEfficacy * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00104() {
+        int luminousEfficacy = 18001;
+
+        LuminousEfficacy result = new LuminousEfficacy(luminousEfficacy);
+        assertEquals(luminousEfficacy, result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MAXIMUM, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00105() {
+        int luminousEfficacy = 1;
+
+        LuminousEfficacy result = new LuminousEfficacy(luminousEfficacy);
+        assertEquals(luminousEfficacy, result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(luminousEfficacy * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00106() {
+        int luminousEfficacy = 17999;
+
+        LuminousEfficacy result = new LuminousEfficacy(luminousEfficacy);
+        assertEquals(luminousEfficacy, result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(luminousEfficacy * LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_constructor_00107() {
+        int luminousEfficacy = -1;
+
+        LuminousEfficacy result = new LuminousEfficacy(luminousEfficacy);
+        assertEquals(luminousEfficacy, result.getLuminousEfficacy());
+        assertFalse(result.isLuminousEfficacyValueIsNotKnown());
+        assertEquals(LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MINIMUM, result.getLuminousEfficacyLumenPerWatt(), 0);
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result1 = new LuminousEfficacy(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MINIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT);
+        data[ 1] = (byte) (((int) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MINIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT)) >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result1 = new LuminousEfficacy(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MAXIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT);
+        data[ 1] = (byte) (((int) (LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_MAXIMUM / LuminousEfficacy.LUMINOUS_EFFICACY_VALUE_UNIT)) >> 8);
+        //@formatter:on
+        LuminousEfficacy result1 = new LuminousEfficacy(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00104() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 18001;
+        data[ 1] = (byte) (18001 >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result1 = new LuminousEfficacy(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00105() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 1;
+        data[ 1] = (byte) (1 >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result1 = new LuminousEfficacy(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00106() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 17999;
+        data[ 1] = (byte) (17999 >> 8);
+        //@formatter:on
+
+        LuminousEfficacy result1 = new LuminousEfficacy(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

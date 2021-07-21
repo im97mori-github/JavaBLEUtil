36e6e24ce4217769a283cb4494710ba6c716280a
im97mori <github@im97mori.org>
2021-07-21 16:38:34 +0900

    update Luminous Flux Range characteristic

--- characteristic/u2b00/src/main/java/org/im97mori/ble/characteristic/u2b00/LuminousFluxRange.java
+++ characteristic/u2b00/src/main/java/org/im97mori/ble/characteristic/u2b00/LuminousFluxRange.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2b00;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,17 @@ import androidx.annotation.NonNull;
 /**
  * Luminous Flux Range (Characteristics UUID: 0x2B00)
  */
-// TODO
 public class LuminousFluxRange implements ByteArrayInterface {
+
+    /**
+     * Minimum Luminous Flux
+     */
+    private final int mMinimumLuminousFlux;
+
+    /**
+     * Maximum Luminous Flux
+     */
+    private final int mMaximumLuminousFlux;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +29,33 @@ public class LuminousFluxRange implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B00
      */
     public LuminousFluxRange(@NonNull byte[] values) {
+        mMinimumLuminousFlux = BLEUtils.createUInt16(values, 0);
+        mMaximumLuminousFlux = BLEUtils.createUInt16(values, 2);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param minimumLuminousFlux Minimum Luminous Flux
+     * @param maximumLuminousFlux Maximum Luminous Flux
+     */
+    public LuminousFluxRange(int minimumLuminousFlux, int maximumLuminousFlux) {
+        mMinimumLuminousFlux = minimumLuminousFlux;
+        mMaximumLuminousFlux = maximumLuminousFlux;
+    }
+
+    /**
+     * @return Minimum Luminous Flux
+     */
+    public int getMinimumLuminousFlux() {
+        return mMinimumLuminousFlux;
+    }
+
+    /**
+     * @return Maximum Luminous Flux
+     */
+    public int getMaximumLuminousFlux() {
+        return mMaximumLuminousFlux;
     }
 
     /**
@@ -24,7 +64,12 @@ public class LuminousFluxRange implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[4];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mMinimumLuminousFlux);
+        byteBuffer.put((byte) (mMinimumLuminousFlux >> 8));
+        byteBuffer.put((byte) mMaximumLuminousFlux);
+        byteBuffer.put((byte) (mMaximumLuminousFlux >> 8));
         return data;
     }
 
---
+++ characteristic/u2b00/src/test/java/org/im97mori/ble/characteristic/u2b00/LuminousFluxRangeTest.java
@@ -0,0 +1,168 @@
+package org.im97mori.ble.characteristic.u2b00;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.characteristic.core.LuminousFluxUtils;
+import org.junit.Test;
+
+public class LuminousFluxRangeTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN;
+        data[ 3] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        LuminousFluxRange result = new LuminousFluxRange(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getMinimumLuminousFlux());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM;
+        data[ 1] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM >> 8);
+        data[ 2] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM;
+        data[ 3] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM >> 8);
+        //@formatter:on
+
+        LuminousFluxRange result = new LuminousFluxRange(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getMinimumLuminousFlux());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM;
+        data[ 1] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM >> 8);
+        data[ 2] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM;
+        data[ 3] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM >> 8);
+        //@formatter:on
+
+        LuminousFluxRange result = new LuminousFluxRange(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getMinimumLuminousFlux());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        //@formatter:on
+
+        LuminousFluxRange result = new LuminousFluxRange(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result.getMinimumLuminousFlux());
+        assertEquals(BLEUtils.createUInt16(data, 2), result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int minimumLuminousFlux = LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN;
+        int maximumLuminousFlux = LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN;
+
+        LuminousFluxRange result = new LuminousFluxRange(minimumLuminousFlux, maximumLuminousFlux);
+        assertEquals(minimumLuminousFlux, result.getMinimumLuminousFlux());
+        assertEquals(maximumLuminousFlux, result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int minimumLuminousFlux = LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM;
+        int maximumLuminousFlux = LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM;
+
+        LuminousFluxRange result = new LuminousFluxRange(minimumLuminousFlux, maximumLuminousFlux);
+        assertEquals(minimumLuminousFlux, result.getMinimumLuminousFlux());
+        assertEquals(maximumLuminousFlux, result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int minimumLuminousFlux = LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM;
+        int maximumLuminousFlux = LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM;
+
+        LuminousFluxRange result = new LuminousFluxRange(minimumLuminousFlux, maximumLuminousFlux);
+        assertEquals(minimumLuminousFlux, result.getMinimumLuminousFlux());
+        assertEquals(maximumLuminousFlux, result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_constructor_00104() {
+        int minimumLuminousFlux = 1;
+        int maximumLuminousFlux = 2;
+
+        LuminousFluxRange result = new LuminousFluxRange(minimumLuminousFlux, maximumLuminousFlux);
+        assertEquals(minimumLuminousFlux, result.getMinimumLuminousFlux());
+        assertEquals(maximumLuminousFlux, result.getMaximumLuminousFlux());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN;
+        data[ 3] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        LuminousFluxRange result1 = new LuminousFluxRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM;
+        data[ 1] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM >> 8);
+        data[ 2] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM;
+        data[ 3] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MINIMUM >> 8);
+        //@formatter:on
+
+        LuminousFluxRange result1 = new LuminousFluxRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM;
+        data[ 1] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM >> 8);
+        data[ 2] = (byte) LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM;
+        data[ 3] = (byte) (LuminousFluxUtils.LUMINOUS_FLUX_VALUE_MAXIMUM >> 8);
+        //@formatter:on
+
+        LuminousFluxRange result1 = new LuminousFluxRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00104() {
+        //@formatter:off
+        byte[] data = new byte[4];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        //@formatter:on
+
+        LuminousFluxRange result1 = new LuminousFluxRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

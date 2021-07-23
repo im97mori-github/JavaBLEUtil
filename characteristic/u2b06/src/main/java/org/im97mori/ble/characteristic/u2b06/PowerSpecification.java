6db8e5c321a12eddf2f117d641f9e78b8f8ab082
im97mori <github@im97mori.org>
2021-07-23 14:33:52 +0900

    update Power Specification characteristic

--- characteristic/u2b06/src/main/java/org/im97mori/ble/characteristic/u2b06/PowerSpecification.java
+++ characteristic/u2b06/src/main/java/org/im97mori/ble/characteristic/u2b06/PowerSpecification.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2b06;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,22 @@ import androidx.annotation.NonNull;
 /**
  * Power Specification (Characteristics UUID: 0x2B06)
  */
-// TODO
 public class PowerSpecification implements ByteArrayInterface {
+
+    /**
+     * Minimum Power Value
+     */
+    private final int mMinimumPowerValue;
+
+    /**
+     * Typical Power Value
+     */
+    private final int mTypicalPowerValue;
+
+    /**
+     * Maximum Power Value
+     */
+    private final int mMaximumPowerValue;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +34,43 @@ public class PowerSpecification implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B06
      */
     public PowerSpecification(@NonNull byte[] values) {
+        mMinimumPowerValue = BLEUtils.createUInt24(values, 0);
+        mTypicalPowerValue = BLEUtils.createUInt24(values, 3);
+        mMaximumPowerValue = BLEUtils.createUInt24(values, 6);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param minimumPowerValue Minimum Power Value
+     * @param typicalPowerValue Typical Power Value
+     * @param maximumPowerValue Maximum Power Value
+     */
+    public PowerSpecification(int minimumPowerValue, int typicalPowerValue, int maximumPowerValue) {
+        mMinimumPowerValue = minimumPowerValue;
+        mTypicalPowerValue = typicalPowerValue;
+        mMaximumPowerValue = maximumPowerValue;
+    }
+
+    /**
+     * @return Minimum Power Value
+     */
+    public int getMinimumPowerValue() {
+        return mMinimumPowerValue;
+    }
+
+    /**
+     * @return Typical Power Value
+     */
+    public int getTypicalPowerValue() {
+        return mTypicalPowerValue;
+    }
+
+    /**
+     * @return Maximum Power Value
+     */
+    public int getMaximumPowerValue() {
+        return mMaximumPowerValue;
     }
 
     /**
@@ -24,7 +79,17 @@ public class PowerSpecification implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[9];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mMinimumPowerValue);
+        byteBuffer.put((byte) (mMinimumPowerValue >> 8));
+        byteBuffer.put((byte) (mMinimumPowerValue >> 16));
+        byteBuffer.put((byte) mTypicalPowerValue);
+        byteBuffer.put((byte) (mTypicalPowerValue >> 8));
+        byteBuffer.put((byte) (mTypicalPowerValue >> 16));
+        byteBuffer.put((byte) mMaximumPowerValue);
+        byteBuffer.put((byte) (mMaximumPowerValue >> 8));
+        byteBuffer.put((byte) (mMaximumPowerValue >> 16));
         return data;
     }
 
---
+++ characteristic/u2b06/src/test/java/org/im97mori/ble/characteristic/u2b06/PowerSpecificationTest.java
@@ -0,0 +1,220 @@
+package org.im97mori.ble.characteristic.u2b06;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.characteristic.core.PowerUtils;
+import org.junit.Test;
+
+public class PowerSpecificationTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = (byte) PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 16);
+        data[ 3] = (byte) PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        data[ 4] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 5] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 16);
+        data[ 6] = (byte) PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        data[ 7] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 8] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 16);
+        //@formatter:on
+
+        PowerSpecification result = new PowerSpecification(data);
+        assertEquals(BLEUtils.createUInt24(data, 0), result.getMinimumPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 3), result.getTypicalPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 6), result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = 0x00;
+        data[ 1] = 0x00;
+        data[ 2] = 0x00;
+        data[ 3] = 0x00;
+        data[ 4] = 0x00;
+        data[ 5] = 0x00;
+        data[ 6] = 0x00;
+        data[ 7] = 0x00;
+        data[ 8] = 0x00;
+        //@formatter:on
+
+        PowerSpecification result = new PowerSpecification(data);
+        assertEquals(BLEUtils.createUInt24(data, 0), result.getMinimumPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 3), result.getTypicalPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 6), result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = (byte) 16777214;
+        data[ 1] = (byte) (16777214 >> 8);
+        data[ 2] = (byte) (16777214 >> 16);
+        data[ 3] = (byte) 16777214;
+        data[ 4] = (byte) (16777214 >> 8);
+        data[ 5] = (byte) (16777214 >> 16);
+        data[ 6] = (byte) 16777214;
+        data[ 7] = (byte) (16777214 >> 8);
+        data[ 8] = (byte) (16777214 >> 16);
+        //@formatter:on
+
+        PowerSpecification result = new PowerSpecification(data);
+        assertEquals(BLEUtils.createUInt24(data, 0), result.getMinimumPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 3), result.getTypicalPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 6), result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        data[ 5] = 0x06;
+        data[ 6] = 0x07;
+        data[ 7] = 0x08;
+        data[ 8] = 0x09;
+        //@formatter:on
+
+        PowerSpecification result = new PowerSpecification(data);
+        assertEquals(BLEUtils.createUInt24(data, 0), result.getMinimumPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 3), result.getTypicalPowerValue());
+        assertEquals(BLEUtils.createUInt24(data, 6), result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int minimumPowerValue = PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        int typicalPowerValue = PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        int maximumPowerValue = PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+
+        PowerSpecification result = new PowerSpecification(minimumPowerValue, typicalPowerValue, maximumPowerValue);
+        assertEquals(minimumPowerValue, result.getMinimumPowerValue());
+        assertEquals(typicalPowerValue, result.getTypicalPowerValue());
+        assertEquals(maximumPowerValue, result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int minimumPowerValue = 0;
+        int typicalPowerValue = 0;
+        int maximumPowerValue = 0;
+
+        PowerSpecification result = new PowerSpecification(minimumPowerValue, typicalPowerValue, maximumPowerValue);
+        assertEquals(minimumPowerValue, result.getMinimumPowerValue());
+        assertEquals(typicalPowerValue, result.getTypicalPowerValue());
+        assertEquals(maximumPowerValue, result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int minimumPowerValue = 16777214;
+        int typicalPowerValue = 16777214;
+        int maximumPowerValue = 16777214;
+
+        PowerSpecification result = new PowerSpecification(minimumPowerValue, typicalPowerValue, maximumPowerValue);
+        assertEquals(minimumPowerValue, result.getMinimumPowerValue());
+        assertEquals(typicalPowerValue, result.getTypicalPowerValue());
+        assertEquals(maximumPowerValue, result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_constructor_00104() {
+        int minimumPowerValue = 1;
+        int typicalPowerValue = 2;
+        int maximumPowerValue = 3;
+
+        PowerSpecification result = new PowerSpecification(minimumPowerValue, typicalPowerValue, maximumPowerValue);
+        assertEquals(minimumPowerValue, result.getMinimumPowerValue());
+        assertEquals(typicalPowerValue, result.getTypicalPowerValue());
+        assertEquals(maximumPowerValue, result.getMaximumPowerValue());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = (byte) PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 16);
+        data[ 3] = (byte) PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        data[ 4] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 5] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 16);
+        data[ 6] = (byte) PowerUtils.POWER_VALUE_IS_NOT_KNOWN;
+        data[ 7] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 8] = (byte) (PowerUtils.POWER_VALUE_IS_NOT_KNOWN >> 16);
+        //@formatter:on
+
+        PowerSpecification result1 = new PowerSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = 0x00;
+        data[ 1] = 0x00;
+        data[ 2] = 0x00;
+        data[ 3] = 0x00;
+        data[ 4] = 0x00;
+        data[ 5] = 0x00;
+        data[ 6] = 0x00;
+        data[ 7] = 0x00;
+        data[ 8] = 0x00;
+        //@formatter:on
+
+        PowerSpecification result1 = new PowerSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = (byte) 16777214;
+        data[ 1] = (byte) (16777214 >> 8);
+        data[ 2] = (byte) (16777214 >> 16);
+        data[ 3] = (byte) 16777214;
+        data[ 4] = (byte) (16777214 >> 8);
+        data[ 5] = (byte) (16777214 >> 16);
+        data[ 6] = (byte) 16777214;
+        data[ 7] = (byte) (16777214 >> 8);
+        data[ 8] = (byte) (16777214 >> 16);
+        //@formatter:on
+
+        PowerSpecification result1 = new PowerSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00104() {
+        //@formatter:off
+        byte[] data = new byte[9];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        data[ 5] = 0x06;
+        data[ 6] = 0x07;
+        data[ 7] = 0x08;
+        data[ 8] = 0x09;
+        //@formatter:on
+
+        PowerSpecification result1 = new PowerSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

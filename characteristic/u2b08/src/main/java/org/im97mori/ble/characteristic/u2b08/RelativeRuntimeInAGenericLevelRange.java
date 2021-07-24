8414eb4026dba0689cd0d0f6ffc18a705fbb7a15
im97mori <github@im97mori.org>
2021-07-24 14:26:24 +0900

    update Relative Runtime In A Generic Level Range characteristic

--- characteristic/u2b08/src/main/java/org/im97mori/ble/characteristic/u2b08/RelativeRuntimeInAGenericLevelRange.java
+++ characteristic/u2b08/src/main/java/org/im97mori/ble/characteristic/u2b08/RelativeRuntimeInAGenericLevelRange.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2b08;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,22 @@ import androidx.annotation.NonNull;
 /**
  * Relative Runtime In A Generic Level Range (Characteristics UUID: 0x2B08)
  */
-// TODO
 public class RelativeRuntimeInAGenericLevelRange implements ByteArrayInterface {
+
+    /**
+     * Relative Value
+     */
+    private final int mRelativeValue;
+
+    /**
+     * Minimum Generic Level
+     */
+    private final int mMinimumGenericLevel;
+
+    /**
+     * Maximum Generic Level
+     */
+    private final int mMaximumGenericLevel;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +34,43 @@ public class RelativeRuntimeInAGenericLevelRange implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B08
      */
     public RelativeRuntimeInAGenericLevelRange(@NonNull byte[] values) {
+        mRelativeValue = BLEUtils.createUInt8(values, 0);
+        mMinimumGenericLevel = BLEUtils.createUInt16(values, 1);
+        mMaximumGenericLevel = BLEUtils.createUInt16(values, 3);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param relativeValue Relative Value
+     * @param minimumGenericLevel Minimum Generic Level
+     * @param maximumGenericLevel Maximum Generic Level
+     */
+    public RelativeRuntimeInAGenericLevelRange(int relativeValue, int minimumGenericLevel, int maximumGenericLevel) {
+        mRelativeValue = relativeValue;
+        mMinimumGenericLevel = minimumGenericLevel;
+        mMaximumGenericLevel = maximumGenericLevel;
+    }
+
+    /**
+     * @return Relative Value
+     */
+    public int getRelativeValue() {
+        return mRelativeValue;
+    }
+
+    /**
+     * @return Minimum Generic Level
+     */
+    public int getMinimumGenericLevel() {
+        return mMinimumGenericLevel;
+    }
+
+    /**
+     * @return Maximum Generic Level
+     */
+    public int getMaximumGenericLevel() {
+        return mMaximumGenericLevel;
     }
 
     /**
@@ -24,7 +79,13 @@ public class RelativeRuntimeInAGenericLevelRange implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[5];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.put((byte) mRelativeValue);
+        byteBuffer.put((byte) mMinimumGenericLevel);
+        byteBuffer.put((byte) (mMinimumGenericLevel >> 8));
+        byteBuffer.put((byte) mMaximumGenericLevel);
+        byteBuffer.put((byte) (mMaximumGenericLevel >> 8));
         return data;
     }
 
---
+++ characteristic/u2b08/src/test/java/org/im97mori/ble/characteristic/u2b08/RelativeRuntimeInAGenericLevelRangeTest.java
@@ -0,0 +1,189 @@
+package org.im97mori.ble.characteristic.u2b08;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.characteristic.core.GenericLevelUtils;
+import org.im97mori.ble.characteristic.core.Percentage8Utils;
+import org.junit.Test;
+
+public class RelativeRuntimeInAGenericLevelRangeTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = (byte) Percentage8Utils.PERCENTAGE_8_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM;
+        data[ 2] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM >> 8);
+        data[ 3] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM;
+        data[ 4] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM >> 8);
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getRelativeValue());
+        assertEquals(BLEUtils.createUInt16(data, 1), result.getMinimumGenericLevel());
+        assertEquals(BLEUtils.createUInt16(data, 3), result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = (byte) Percentage8Utils.PERCENTAGE_8_VALUE_MINIMUM;
+        data[ 1] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM;
+        data[ 2] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM >> 8);
+        data[ 3] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM;
+        data[ 4] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM >> 8);
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getRelativeValue());
+        assertEquals(BLEUtils.createUInt16(data, 1), result.getMinimumGenericLevel());
+        assertEquals(BLEUtils.createUInt16(data, 3), result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = (byte) Percentage8Utils.PERCENTAGE_8_VALUE_MAXIMUM;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getRelativeValue());
+        assertEquals(BLEUtils.createUInt16(data, 1), result.getMinimumGenericLevel());
+        assertEquals(BLEUtils.createUInt16(data, 3), result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(data);
+        assertEquals(BLEUtils.createUInt8(data, 0), result.getRelativeValue());
+        assertEquals(BLEUtils.createUInt16(data, 1), result.getMinimumGenericLevel());
+        assertEquals(BLEUtils.createUInt16(data, 3), result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int relativeValue = Percentage8Utils.PERCENTAGE_8_VALUE_IS_NOT_KNOWN;
+        int minimumGenericLevel = GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM;
+        int maximumGenericLevel = GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM;
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(relativeValue, minimumGenericLevel, maximumGenericLevel);
+        assertEquals(relativeValue, result.getRelativeValue());
+        assertEquals(minimumGenericLevel, result.getMinimumGenericLevel());
+        assertEquals(maximumGenericLevel, result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int relativeValue = (int) Percentage8Utils.PERCENTAGE_8_VALUE_MINIMUM;
+        int minimumGenericLevel = GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM;
+        int maximumGenericLevel = GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM;
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(relativeValue, minimumGenericLevel, maximumGenericLevel);
+        assertEquals(relativeValue, result.getRelativeValue());
+        assertEquals(minimumGenericLevel, result.getMinimumGenericLevel());
+        assertEquals(maximumGenericLevel, result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int relativeValue = (int) Percentage8Utils.PERCENTAGE_8_VALUE_MAXIMUM;
+        int minimumGenericLevel = 2;
+        int maximumGenericLevel = 3;
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(relativeValue, minimumGenericLevel, maximumGenericLevel);
+        assertEquals(relativeValue, result.getRelativeValue());
+        assertEquals(minimumGenericLevel, result.getMinimumGenericLevel());
+        assertEquals(maximumGenericLevel, result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_constructor_00104() {
+        int relativeValue = 1;
+        int minimumGenericLevel = 2;
+        int maximumGenericLevel = 3;
+
+        RelativeRuntimeInAGenericLevelRange result = new RelativeRuntimeInAGenericLevelRange(relativeValue, minimumGenericLevel, maximumGenericLevel);
+        assertEquals(relativeValue, result.getRelativeValue());
+        assertEquals(minimumGenericLevel, result.getMinimumGenericLevel());
+        assertEquals(maximumGenericLevel, result.getMaximumGenericLevel());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = (byte) Percentage8Utils.PERCENTAGE_8_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM;
+        data[ 2] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM >> 8);
+        data[ 3] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM;
+        data[ 4] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MINIMUM >> 8);
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result1 = new RelativeRuntimeInAGenericLevelRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = (byte) Percentage8Utils.PERCENTAGE_8_VALUE_MINIMUM;
+        data[ 1] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM;
+        data[ 2] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM >> 8);
+        data[ 3] = (byte) GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM;
+        data[ 4] = (byte) (GenericLevelUtils.GENERIC_LEVEL_VALUE_MAXIMUM >> 8);
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result1 = new RelativeRuntimeInAGenericLevelRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = (byte) Percentage8Utils.PERCENTAGE_8_VALUE_MAXIMUM;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result1 = new RelativeRuntimeInAGenericLevelRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00104() {
+        //@formatter:off
+        byte[] data = new byte[5];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        //@formatter:on
+
+        RelativeRuntimeInAGenericLevelRange result1 = new RelativeRuntimeInAGenericLevelRange(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}

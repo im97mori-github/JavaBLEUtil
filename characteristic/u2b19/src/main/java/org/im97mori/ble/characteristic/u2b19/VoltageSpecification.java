a8358b651fb2b9851fd8293bad8af83f3093ebcd
im97mori <github@im97mori.org>
2021-07-28 14:12:05 +0900

    update Voltage Specification characteristic

--- characteristic/u2b19/src/main/java/org/im97mori/ble/characteristic/u2b19/VoltageSpecification.java
+++ characteristic/u2b19/src/main/java/org/im97mori/ble/characteristic/u2b19/VoltageSpecification.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2b19;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,22 @@ import androidx.annotation.NonNull;
 /**
  * Voltage Specification (Characteristics UUID: 0x2B19)
  */
-// TODO
 public class VoltageSpecification implements ByteArrayInterface {
+
+    /**
+     * Minimum Voltage Value
+     */
+    private final int mMinimumVoltageValue;
+
+    /**
+     * Typical Voltage Value
+     */
+    private final int mTypicalVoltageValue;
+
+    /**
+     * Maximum Voltage Value
+     */
+    private final int mMaximumVoltageValue;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +34,43 @@ public class VoltageSpecification implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B19
      */
     public VoltageSpecification(@NonNull byte[] values) {
+        mMinimumVoltageValue = BLEUtils.createUInt16(values, 0);
+        mTypicalVoltageValue = BLEUtils.createUInt16(values, 2);
+        mMaximumVoltageValue = BLEUtils.createUInt16(values, 4);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param minimumVoltageValue Minimum Voltage Value
+     * @param typicalVoltageValue Typical Voltage Value
+     * @param maximumVoltageValue Maximum Voltage Value
+     */
+    public VoltageSpecification(int minimumVoltageValue, int typicalVoltageValue, int maximumVoltageValue) {
+        mMinimumVoltageValue = minimumVoltageValue;
+        mTypicalVoltageValue = typicalVoltageValue;
+        mMaximumVoltageValue = maximumVoltageValue;
+    }
+
+    /**
+     * @return Minimum Voltage Value
+     */
+    public int getMinimumVoltageValue() {
+        return mMinimumVoltageValue;
+    }
+
+    /**
+     * @return Typical Voltage Value
+     */
+    public int getTypicalVoltageValue() {
+        return mTypicalVoltageValue;
+    }
+
+    /**
+     * @return Maximum Voltage Value
+     */
+    public int getMaximumVoltageValue() {
+        return mMaximumVoltageValue;
     }
 
     /**
@@ -24,7 +79,11 @@ public class VoltageSpecification implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[6];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.putShort((short) mMinimumVoltageValue);
+        byteBuffer.putShort((short) mTypicalVoltageValue);
+        byteBuffer.putShort((short) mMaximumVoltageValue);
         return data;
     }
 
---
+++ characteristic/u2b19/src/test/java/org/im97mori/ble/characteristic/u2b19/VoltageSpecificationTest.java
@@ -0,0 +1,196 @@
+package org.im97mori.ble.characteristic.u2b19;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.im97mori.ble.BLEUtils;
+import org.im97mori.ble.characteristic.core.VoltageUtils;
+import org.junit.Test;
+
+public class VoltageSpecificationTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        data[ 3] = (byte) (VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 4] = (byte) VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        data[ 5] = (byte) (VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertEquals(VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN, result1.getMinimumVoltageValue());
+        assertEquals(VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN, result1.getTypicalVoltageValue());
+        assertEquals(VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN, result1.getMaximumVoltageValue());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0;
+        data[ 1] = 0;
+        data[ 2] = 0;
+        data[ 3] = 0;
+        data[ 4] = 0;
+        data[ 5] = 0;
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertEquals(0, result1.getMinimumVoltageValue());
+        assertEquals(0, result1.getTypicalVoltageValue());
+        assertEquals(0, result1.getMaximumVoltageValue());
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) 65408;
+        data[ 1] = (byte) (65408 >> 8);
+        data[ 2] = (byte) 65408;
+        data[ 3] = (byte) (65408 >> 8);
+        data[ 4] = (byte) 65408;
+        data[ 5] = (byte) (65408 >> 8);
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertEquals(65408, result1.getMinimumVoltageValue());
+        assertEquals(65408, result1.getTypicalVoltageValue());
+        assertEquals(65408, result1.getMaximumVoltageValue());
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        data[ 5] = 0x06;
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result1.getMinimumVoltageValue());
+        assertEquals(BLEUtils.createUInt16(data, 2), result1.getTypicalVoltageValue());
+        assertEquals(BLEUtils.createUInt16(data, 4), result1.getMaximumVoltageValue());
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int minimumVoltageValue = VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        int typicalVoltageValue = VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        int maximumVoltageValue = VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+
+        VoltageSpecification result1 = new VoltageSpecification(minimumVoltageValue, typicalVoltageValue, maximumVoltageValue);
+        assertEquals(minimumVoltageValue, result1.getMinimumVoltageValue());
+        assertEquals(typicalVoltageValue, result1.getTypicalVoltageValue());
+        assertEquals(maximumVoltageValue, result1.getMaximumVoltageValue());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int minimumVoltageValue = 0;
+        int typicalVoltageValue = 0;
+        int maximumVoltageValue = 0;
+
+        VoltageSpecification result1 = new VoltageSpecification(minimumVoltageValue, typicalVoltageValue, maximumVoltageValue);
+        assertEquals(minimumVoltageValue, result1.getMinimumVoltageValue());
+        assertEquals(typicalVoltageValue, result1.getTypicalVoltageValue());
+        assertEquals(maximumVoltageValue, result1.getMaximumVoltageValue());
+    }
+    
+    @Test
+    public void test_constructor_00103() {
+        int minimumVoltageValue = 65408;
+        int typicalVoltageValue = 65408;
+        int maximumVoltageValue = 65408;
+
+        VoltageSpecification result1 = new VoltageSpecification(minimumVoltageValue, typicalVoltageValue, maximumVoltageValue);
+        assertEquals(minimumVoltageValue, result1.getMinimumVoltageValue());
+        assertEquals(typicalVoltageValue, result1.getTypicalVoltageValue());
+        assertEquals(maximumVoltageValue, result1.getMaximumVoltageValue());
+    }
+    
+    @Test
+    public void test_constructor_00104() {
+        int minimumVoltageValue = 1;
+        int typicalVoltageValue = 2;
+        int maximumVoltageValue = 3;
+
+        VoltageSpecification result1 = new VoltageSpecification(minimumVoltageValue, typicalVoltageValue, maximumVoltageValue);
+        assertEquals(minimumVoltageValue, result1.getMinimumVoltageValue());
+        assertEquals(typicalVoltageValue, result1.getTypicalVoltageValue());
+        assertEquals(maximumVoltageValue, result1.getMaximumVoltageValue());
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 2] = (byte) VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        data[ 3] = (byte) (VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN >> 8);
+        data[ 4] = (byte) VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN;
+        data[ 5] = (byte) (VoltageUtils.VOLTAGE_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0;
+        data[ 1] = 0;
+        data[ 2] = 0;
+        data[ 3] = 0;
+        data[ 4] = 0;
+        data[ 5] = 0;
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+    
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = (byte) 65408;
+        data[ 1] = (byte) (65408 >> 8);
+        data[ 2] = (byte) 65408;
+        data[ 3] = (byte) (65408 >> 8);
+        data[ 4] = (byte) 65408;
+        data[ 5] = (byte) (65408 >> 8);
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00104() {
+        //@formatter:off
+        byte[] data = new byte[6];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        data[ 2] = 0x03;
+        data[ 3] = 0x04;
+        data[ 4] = 0x05;
+        data[ 5] = 0x06;
+        //@formatter:on
+
+        VoltageSpecification result1 = new VoltageSpecification(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+    
+}

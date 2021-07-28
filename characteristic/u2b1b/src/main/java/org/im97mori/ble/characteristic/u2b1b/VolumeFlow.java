fba7ceac079602c6391286623fe91ea75eb7b7ae
im97mori <github@im97mori.org>
2021-07-28 17:59:15 +0900

    update Volume Flow characteristic

--- characteristic/u2b1b/src/main/java/org/im97mori/ble/characteristic/u2b1b/VolumeFlow.java
+++ characteristic/u2b1b/src/main/java/org/im97mori/ble/characteristic/u2b1b/VolumeFlow.java
@@ -1,5 +1,9 @@
 package org.im97mori.ble.characteristic.u2b1b;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import org.im97mori.ble.BLEUtils;
 import org.im97mori.ble.ByteArrayInterface;
 
 import androidx.annotation.NonNull;
@@ -7,8 +11,32 @@ import androidx.annotation.NonNull;
 /**
  * Volume Flow (Characteristics UUID: 0x2B1B)
  */
-// TODO
 public class VolumeFlow implements ByteArrayInterface {
+
+    /**
+     * 0xFFFF : A raw value of 0xFFFF represents ‘value is not known’
+     */
+    public static final int VOLUME_FLOW_VALUE_IS_NOT_KNOWN = 0xFFFF;
+
+    /**
+     * Unit is liter/second with a resolution of .001 (1 milliliter)
+     */
+    public static final double VOLUME_FLOW_VALUE_UNIT = 0.001d;
+
+    /**
+     * Volume Flow Minimum value
+     */
+    public static final double VOLUME_FLOW_VALUE_MINIMUM = 0.0d;
+
+    /**
+     * Volume Flow Maximum value
+     */
+    public static final double VOLUME_FLOW_VALUE_MAXIMUM = 65.534d;
+
+    /**
+     * Volume Flow
+     */
+    private final int mVolumeFlow;
 
     /**
      * Constructor from {@link BluetoothGattCharacteristic}
@@ -16,6 +44,44 @@ public class VolumeFlow implements ByteArrayInterface {
      * @param bluetoothGattCharacteristic Characteristics UUID: 0x2B1B
      */
     public VolumeFlow(@NonNull byte[] values) {
+        mVolumeFlow = BLEUtils.createUInt16(values, 0);
+    }
+
+    /**
+     * Constructor from parameters
+     * 
+     * @param volumeFlow Volume Flow
+     */
+    public VolumeFlow(int volumeFlow) {
+        mVolumeFlow = volumeFlow;
+    }
+
+    /**
+     * @return Volume Flow
+     */
+    public int getVolumeFlow() {
+        return mVolumeFlow;
+    }
+
+    /**
+     * @return {@code true}:Volume Flow value is not known, {@code false}:has Volume Flow value information
+     * @see #VOLUME_FLOW_VALUE_IS_NOT_KNOWN
+     */
+    public boolean isVolumeFlowValueIsNotKnown() {
+        return VOLUME_FLOW_VALUE_IS_NOT_KNOWN == mVolumeFlow;
+    }
+
+    /**
+     * @return Volume Flow
+     */
+    public double getVolumeFlowLitterPerSecond() {
+        double volumeFlowLitterPerSecond = mVolumeFlow * VOLUME_FLOW_VALUE_UNIT;
+        if (volumeFlowLitterPerSecond < VOLUME_FLOW_VALUE_MINIMUM) {
+            volumeFlowLitterPerSecond = VOLUME_FLOW_VALUE_MINIMUM;
+        } else if (volumeFlowLitterPerSecond > VOLUME_FLOW_VALUE_MAXIMUM) {
+            volumeFlowLitterPerSecond = VOLUME_FLOW_VALUE_MAXIMUM;
+        }
+        return volumeFlowLitterPerSecond;
     }
 
     /**
@@ -24,7 +90,9 @@ public class VolumeFlow implements ByteArrayInterface {
     @Override
     @NonNull
     public byte[] getBytes() {
-        byte[] data = new byte[0];
+        byte[] data = new byte[2];
+        ByteBuffer byteBuffer = ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN);
+        byteBuffer.putShort((short) mVolumeFlow);
         return data;
     }
 
---
+++ characteristic/u2b1b/src/test/java/org/im97mori/ble/characteristic/u2b1b/VolumeFlowTest.java
@@ -0,0 +1,155 @@
+package org.im97mori.ble.characteristic.u2b1b;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.im97mori.ble.BLEUtils;
+import org.junit.Test;
+
+public class VolumeFlowTest {
+
+    @Test
+    public void test_constructor_00001() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) VolumeFlow.VOLUME_FLOW_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (VolumeFlow.VOLUME_FLOW_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_IS_NOT_KNOWN, result1.getVolumeFlow());
+        assertTrue(result1.isVolumeFlowValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00002() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = 0;
+        data[ 1] = 0;
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result1.getVolumeFlow());
+        assertFalse(result1.isVolumeFlowValueIsNotKnown());
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_MINIMUM, result1.getVolumeFlowLitterPerSecond(), 0);
+    }
+
+    @Test
+    public void test_constructor_00003() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 65534;
+        data[ 1] = (byte) (65534 >> 8);
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result1.getVolumeFlow());
+        assertFalse(result1.isVolumeFlowValueIsNotKnown());
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_MAXIMUM, result1.getVolumeFlowLitterPerSecond(), 0);
+    }
+
+    @Test
+    public void test_constructor_00004() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertEquals(BLEUtils.createUInt16(data, 0), result1.getVolumeFlow());
+        assertFalse(result1.isVolumeFlowValueIsNotKnown());
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_UNIT * BLEUtils.createUInt16(data, 0), result1.getVolumeFlowLitterPerSecond(), 0);
+    }
+
+    @Test
+    public void test_constructor_00101() {
+        int volumeFlow = VolumeFlow.VOLUME_FLOW_VALUE_IS_NOT_KNOWN;
+
+        VolumeFlow result1 = new VolumeFlow(volumeFlow);
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_IS_NOT_KNOWN, result1.getVolumeFlow());
+        assertTrue(result1.isVolumeFlowValueIsNotKnown());
+    }
+
+    @Test
+    public void test_constructor_00102() {
+        int volumeFlow = 0;
+
+        VolumeFlow result1 = new VolumeFlow(volumeFlow);
+        assertEquals(volumeFlow, result1.getVolumeFlow());
+        assertFalse(result1.isVolumeFlowValueIsNotKnown());
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_MINIMUM, result1.getVolumeFlowLitterPerSecond(), 0);
+    }
+
+    @Test
+    public void test_constructor_00103() {
+        int volumeFlow = 65534;
+
+        VolumeFlow result1 = new VolumeFlow(volumeFlow);
+        assertEquals(volumeFlow, result1.getVolumeFlow());
+        assertFalse(result1.isVolumeFlowValueIsNotKnown());
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_MAXIMUM, result1.getVolumeFlowLitterPerSecond(), 0);
+    }
+
+    @Test
+    public void test_constructor_00104() {
+        int volumeFlow = 1;
+
+        VolumeFlow result1 = new VolumeFlow(volumeFlow);
+        assertEquals(volumeFlow, result1.getVolumeFlow());
+        assertFalse(result1.isVolumeFlowValueIsNotKnown());
+        assertEquals(VolumeFlow.VOLUME_FLOW_VALUE_UNIT * volumeFlow, result1.getVolumeFlowLitterPerSecond(), 0);
+    }
+
+    @Test
+    public void test_parcelable_00101() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) VolumeFlow.VOLUME_FLOW_VALUE_IS_NOT_KNOWN;
+        data[ 1] = (byte) (VolumeFlow.VOLUME_FLOW_VALUE_IS_NOT_KNOWN >> 8);
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00102() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = 0;
+        data[ 1] = 0;
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00103() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = (byte) 65534;
+        data[ 1] = (byte) (65534 >> 8);
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+    @Test
+    public void test_parcelable_00104() {
+        //@formatter:off
+        byte[] data = new byte[2];
+        data[ 0] = 0x01;
+        data[ 1] = 0x02;
+        //@formatter:on
+
+        VolumeFlow result1 = new VolumeFlow(data);
+        assertArrayEquals(data, result1.getBytes());
+    }
+
+}
